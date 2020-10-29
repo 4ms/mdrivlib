2636863db282e90fbefb2fa468eeda25715146a8
Dan Green <danngreen1@gmail.com>
2020-10-29 12:17:35 -0700

    Debounced switch polarity=normal doesn't need a pulldown

--- drivers/debounced_switch.cc
+++ drivers/debounced_switch.cc
@@ -1,13 +1,17 @@
 #include "debounced_switch.hh"
 
 DebouncedSwitch::DebouncedSwitch(GPIO port, uint8_t pin_num, PinPolarity polarity)
-	: pin_(port, pin_num, PinMode::Input, LL_GPIO_AF_0, polarity == PinPolarity::Inverted ? PinPull::Up : PinPull::Down, polarity)
+	: pin_(port,
+		   pin_num,
+		   PinMode::Input,
+		   LL_GPIO_AF_0,
+		   polarity == PinPolarity::Inverted ? PinPull::Up : PinPull::None,
+		   polarity)
 	, debounce_state_{0}
 	, pressed_{false}
 	, just_pressed_{false}
 	, just_released_{false}
-{
-}
+{}
 
 void DebouncedSwitch::update()
 {
@@ -17,8 +21,7 @@ void DebouncedSwitch::update()
 		just_pressed_ = true;
 		pressed_ = true;
 		just_released_ = false;
-	}
-	else if (debounce_state_ == 0b1111111100000000) {
+	} else if (debounce_state_ == 0b1111111100000000) {
 		just_pressed_ = false;
 		pressed_ = false;
 		just_released_ = true;
@@ -35,8 +38,7 @@ bool DebouncedSwitch::is_just_pressed()
 	if (just_pressed_) {
 		just_pressed_ = false;
 		return true;
-	}
-	else
+	} else
 		return false;
 }
 
@@ -45,8 +47,7 @@ bool DebouncedSwitch::is_just_released()
 	if (just_released_) {
 		just_released_ = false;
 		return true;
-	}
-	else
+	} else
 		return false;
 }
 
