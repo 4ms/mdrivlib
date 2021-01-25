8e91a521b1564eacedf38af093b707756c5548d9
Dan Green <danngreen1@gmail.com>
2021-01-25 09:38:19 -0800

    WIP: SPI driver for MAX11666 ADC chip

--- drivers/adc_spi_max11666.hh
+++ drivers/adc_spi_max11666.hh
@@ -1,20 +1,78 @@
 #pragma once
+#include "drivers/pin.hh"
 #include "stm32xx.h"
 
-struct SPI_ADC_Config {
+// spi.hh:
+#include "interrupt.hh"
+
+// template<unsigned NUM_CHIPS = 1>
+// struct SpiPins {
+// 	PinNoInit SCLK;
+// 	PinNoInit COPI;
+// 	PinNoInit CIPO;
+// 	PinNoInit CS[NUM_CHIPS];
+// };
+
+template<unsigned NUM_CHIPS = 1>
+struct SpiConfig {
 	SPI_TypeDef *spi;
-	// pins:
-	// with multiple CS pins (max 8)
-	unsigned num_chips;
-	unsigned chans_per_chip;
-	uint32_t change_channel_commands[8];
-	uint32_t continue_channel_commands[8];
+	IRQType IRQn;
+
+	PinNoInit SCLK;
+	PinNoInit COPI;
+	PinNoInit CIPO;
+	PinNoInit CS[NUM_CHIPS];
+
+	// SpiPins<NUM_CHIPS> pins;
+
+	// unsigned num_chips;
+	// unsigned chans_per_chip;
+	// uint32_t change_channel_commands[NUM_CHIPS];
+	// uint32_t continue_channel_commands[NUM_CHIPS];
 };
 
-struct AdcSpi_MAX11666 {
-	AdcSpi_MAX11666(const SPI_ADC_Config conf) {
-		// do stuff with conf...
+template<unsigned NUM_CHIPS = 1>
+class SpiPeriph {
+public:
+	SpiPeriph(const SpiConfig<NUM_CHIPS> &conf)
+		: _spi{conf.spi} {}
+
+	void init() {
+		Pin sclk{_conf.SCLK};
+		Pin copi{_conf.COPI};
+		Pin cipo{_conf.CIPO};
+		for (int i = 0; i < NUM_CHIPS; i++)
+			CS[i].init(_conf.CS[i]);
 	}
-	uint16_t get_val(int chan) { return 0; }
+	void init_IT() {}
+	void enable() {}
+	void disable() {}
+	void send(uint16_t data) { _spi->TXDR = data; }
+	void select(int chip_num) { CS[chip_num].low(); }
+	void unselect(int chip_num) { CS[chip_num].high(); }
+
+private:
+	SPI_TypeDef *_spi;
+	const SpiConfig<NUM_CHIPS> &_conf;
+	Pin CS[NUM_CHIPS];
+};
+// end spi.hh
+
+template<unsigned NUM_CHIPS = 1>
+struct AdcSpi_MAX11666 {
+	AdcSpi_MAX11666(const SpiConfig<NUM_CHIPS> &conf)
+		: spi{conf} {
+		InterruptManager::registerISR(conf.IRQn, [this]() {
+			// Handle ISR here
+		});
+	}
+	void start() {}
+
+	uint16_t get_val(int chan) { return val[chan]; }
+	void advance_channel() {}
+
+private:
+	SpiPeriph<NUM_CHIPS> spi;
+	uint16_t val[NUM_CHIPS * 2];
 };
 
--- drivers/analog_in_ext.hh
+++ drivers/analog_in_ext.hh
@@ -1,4 +1,7 @@
 #pragma once
+#include <algorithm>
+
+// Todo: is this necessary?
 
 // requires SOURCE::get_val(int)-->T, POSTFILTER::add_val(T), POSTFILTER::val()-->FilteredT
 template<typename SOURCE, int NUM_CHANS, typename POSTFILTER>
@@ -6,10 +9,14 @@ struct AnalogIn : SOURCE {
 	AnalogIn() {}
 
 	template<typename T>
-	AnalogIn(T init)
+	AnalogIn(T &&init)
 		: SOURCE{std::move(init)} {}
 
 	void read(int chan) { oversampler_[chan].add_val(this->get_val(chan)); }
+	void read() {
+		for (int chan = 0; chan < NUM_CHANS; chan++)
+			read(chan);
+	}
 	auto get(int chan) { return oversampler_[chan].val(); }
 
 private:
