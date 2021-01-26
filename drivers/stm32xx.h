7b9ae27af2e086d1fef8d352ba8629053087059e
Dan Green <danngreen1@gmail.com>
2021-01-25 16:48:53 -0800

    H7 only: System/RCC: using more streamlined rcc enable/disable
    All periphs not #defined in CMSIS device are #defined as nullptr. This avoids compile errors and lots of pre-processor stuff

--- drivers/stm32xx.h
+++ drivers/stm32xx.h
@@ -21,3 +21,113 @@
 #endif
 #endif
 
+#ifndef ADC1
+#define ADC1 nullptr
+#endif
+#ifndef ADC2
+#define ADC2 nullptr
+#endif
+#ifndef ADC3
+#define ADC3 nullptr
+#endif
+
+#ifndef DMA1
+#define DMA1 nullptr
+#endif
+#ifndef DMA2
+#define DMA2 nullptr
+#endif
+
+#ifndef I2C1
+#define I2C1 nullptr
+#endif
+#ifndef I2C2
+#define I2C2 nullptr
+#endif
+#ifndef I2C3
+#define I2C3 nullptr
+#endif
+
+#ifndef SAI1
+#define SAI1 nullptr
+#endif
+#ifndef SAI2
+#define SAI2 nullptr
+#endif
+#ifndef SAI3
+#define SAI3 nullptr
+#endif
+#ifndef SAI4
+#define SAI4 nullptr
+#endif
+
+#ifndef TIM1
+#define TIM1 nullptr
+#endif
+#ifndef TIM2
+#define TIM2 nullptr
+#endif
+#ifndef TIM3
+#define TIM3 nullptr
+#endif
+#ifndef TIM4
+#define TIM4 nullptr
+#endif
+#ifndef TIM5
+#define TIM5 nullptr
+#endif
+#ifndef TIM6
+#define TIM6 nullptr
+#endif
+#ifndef TIM7
+#define TIM7 nullptr
+#endif
+#ifndef TIM8
+#define TIM8 nullptr
+#endif
+#ifndef TIM9
+#define TIM9 nullptr
+#endif
+#ifndef TIM10
+#define TIM10 nullptr
+#endif
+#ifndef TIM11
+#define TIM11 nullptr
+#endif
+#ifndef TIM12
+#define TIM12 nullptr
+#endif
+#ifndef TIM13
+#define TIM13 nullptr
+#endif
+#ifndef TIM14
+#define TIM14 nullptr
+#endif
+#ifndef TIM15
+#define TIM15 nullptr
+#endif
+#ifndef TIM16
+#define TIM16 nullptr
+#endif
+#ifndef TIM17
+#define TIM17 nullptr
+#endif
+
+#ifndef SPI1
+#define SPI1 nullptr
+#endif
+#ifndef SPI2
+#define SPI2 nullptr
+#endif
+#ifndef SPI3
+#define SPI3 nullptr
+#endif
+#ifndef SPI4
+#define SPI4 nullptr
+#endif
+#ifndef SPI5
+#define SPI5 nullptr
+#endif
+#ifndef SPI6
+#define SPI6 nullptr
+#endif
