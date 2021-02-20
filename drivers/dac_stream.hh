f7f0a21e8811ab445fd1380b36af8396e8732dc4
Dan Green <danngreen1@gmail.com>
2021-02-19 17:22:15 -0800

    dac stream functional

--- drivers/dac_stream.hh
+++ drivers/dac_stream.hh
@@ -4,9 +4,15 @@
 template<typename DACType, typename BufferT>
 struct DacStream : DACType {
 	DacStream() {
+		dac_buffer.set_head(dac_buffer.capacity() / 2);
 	}
 
-	void queue_sample(uint32_t channel, uint32_t value) {
+	void queue_sample(uint32_t value) {
+		dac_buffer.put(value);
+	}
+
+	void output_next(uint32_t chan) {
+		this->set_output_blocking(chan, dac_buffer.get());
 	}
 
 	static inline BufferT dac_buffer;
