e9aba0dfd90ed58512b805709e4ad18b485cf717
Dan Green <danngreen1@gmail.com>
2021-03-05 17:06:55 -0800

    cleaning up mem xfer

--- drivers/memory_transfer.hh
+++ drivers/memory_transfer.hh
@@ -7,9 +7,9 @@
 
 struct MemoryTransfer {
 	MDMA_HandleTypeDef hmdma;
-	uint32_t src_addr;
-	uint32_t dst_addr;
-	uint32_t xfer_size;
+	uint32_t _src_addr;
+	uint32_t _dst_addr;
+	uint32_t _transfer_size;
 
 	// template<typename CallbackT>
 	// void registerCallback(CallbackT &&callback) {
@@ -44,6 +44,11 @@ struct MemoryTransfer {
 		static constexpr uint32_t CCR_Base = MDMA_Channel0_BASE + offsetof(MDMA_Channel_TypeDef, CCR);
 		using Enable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_EN_Pos, 1>;
 		using SWRequest = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_SWRQ_Pos, 1>;
+		using TransferErrISREnable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_TEIE_Pos, 1>;
+		using BufferTransferComplISREnable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_TCIE_Pos, 1>;
+		using BlockTransferComplISREnable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_BTIE_Pos, 1>;
+		using BlockRepeatTransferComplISREnable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_BRTIE_Pos, 1>;
+		using ChannelTransferComplISREnable = RegisterSection<ReadWrite, CCR_Base, MDMA_CCR_CTCIE_Pos, 1>;
 
 		using DstAddr = RegisterBits<ReadWrite, MDMA_Channel0_BASE + offsetof(MDMA_Channel_TypeDef, CDAR), 0xFFFFFFFF>;
 		using SrcAddr = RegisterBits<ReadWrite, MDMA_Channel0_BASE + offsetof(MDMA_Channel_TypeDef, CSAR), 0xFFFFFFFF>;
@@ -56,9 +61,14 @@ struct MemoryTransfer {
 		using RequestIsActive = RegisterSection<ReadOnly, CISR_Base, MDMA_CISR_CRQA_Pos, 1>;
 	};
 
-	void first_start(void *dst, const void *src, size_t sz) {
+	void config_transfer(void *dst, const void *src, size_t sz) {
+		_transfer_size = sz;
+		_src_addr = reinterpret_cast<uint32_t>(src);
+		_dst_addr = reinterpret_cast<uint32_t>(dst);
+
 		target::RCC_Control::MDMA_::set();
 
+		// Todo: use the registers Luke!
 		hmdma.Instance = MDMA_Channel0;
 		hmdma.Init.Request = MDMA_REQUEST_SW;
 		hmdma.Init.TransferTriggerMode = MDMA_BUFFER_TRANSFER;
@@ -69,7 +79,7 @@ struct MemoryTransfer {
 		hmdma.Init.SourceDataSize = MDMA_SRC_DATASIZE_WORD;
 		hmdma.Init.DestDataSize = MDMA_DEST_DATASIZE_WORD;
 		hmdma.Init.DataAlignment = MDMA_DATAALIGN_PACKENABLE;
-		hmdma.Init.BufferTransferLength = sz;
+		hmdma.Init.BufferTransferLength = _transfer_size;
 		hmdma.Init.SourceBurst = MDMA_SOURCE_BURST_SINGLE;
 		hmdma.Init.DestBurst = MDMA_DEST_BURST_SINGLE;
 		hmdma.Init.SourceBlockAddressOffset = 0;
@@ -84,30 +94,26 @@ struct MemoryTransfer {
 		if (err != HAL_OK)
 			__BKPT();
 
-		xfer_size = sz;
 		MDMA0::Enable::clear();
-		MDMA0::BlockNumDataBytesToXfer::write(xfer_size);
+		MDMA0::BlockNumDataBytesToXfer::write(_transfer_size);
 		MDMA0::BlockRepeatCount::write(0);
 		MDMA_Channel0->CIFCR =
 			MDMA_CIFCR_CBRTIF | MDMA_CIFCR_CTEIF | MDMA_CIFCR_CCTCIF | MDMA_CIFCR_CLTCIF | MDMA_CIFCR_CBTIF;
 
-		src_addr = reinterpret_cast<uint32_t>(src);
-		dst_addr = reinterpret_cast<uint32_t>(dst);
 		_set_addrs();
 		_set_addr_bus_bits();
 
+		// Todo: check if size>127bytes (multi-buffer block), the right ISR is set
 		MDMA_Channel0->CCR =
 			MDMA_Channel0->CCR | MDMA_CCR_TEIE | MDMA_CCR_TCIE | MDMA_CCR_BTIE; // | MDMA_CCR_BRTIE | MDMA_CCR_CTCIE;
-
-		repeat_start();
 	}
 
-	void repeat_start() {
+	void start_transfer() {
 		MDMA0::Enable::clear();
-		MDMA0::BlockNumDataBytesToXfer::write(xfer_size);
-		MDMA0::BlockRepeatCount::write(0);
+		MDMA0::BlockNumDataBytesToXfer::write(_transfer_size);
 		_set_addrs();
 		MDMA0::Enable::set();
+
 		// Debug point:
 		if (MDMA0::RequestIsActive::read())
 			while (1)
@@ -115,29 +121,17 @@ struct MemoryTransfer {
 		MDMA0::SWRequest::set();
 	}
 
+private:
 	void _set_addrs() {
-		MDMA0::SrcAddr::write(src_addr);
-		MDMA0::DstAddr::write(dst_addr);
+		MDMA0::SrcAddr::write(_src_addr);
+		MDMA0::DstAddr::write(_dst_addr);
 	}
 
 	void _set_addr_bus_bits() {
-		uint32_t addressMask;
-		addressMask = src_addr & 0xFF000000U;
-		if ((addressMask == 0x20000000U) || (addressMask == 0x00000000U)) {
-			MDMA0::SrcBusIsAHBTCM::set();
-		} else {
-			MDMA0::SrcBusIsAHBTCM::clear();
-		}
+		const uint32_t src_bus = _src_addr & 0xFF000000;
+		MDMA0::SrcBusIsAHBTCM::write(src_bus == 0x20000000 || src_bus == 0);
 
-		addressMask = dst_addr & 0xFF000000U;
-		if ((addressMask == 0x20000000U) || (addressMask == 0x00000000U)) {
-			MDMA0::DstBusIsAHBTCM::set();
-		} else {
-			MDMA0::DstBusIsAHBTCM::clear();
-		}
-		// const uint32_t src_bus = src_addr & 0xFF000000U;
-		// MDMA0::SrcBusIsAHBTCM::write(src_bus == 0x20000000 || src_bus == 0);
-		// const uint32_t dsr_bus = dst_addr & 0xFF000000U;
-		// MDMA0::DstBusIsAHBTCM::write(dst_bus == 0x20000000 || dst_bus == 0);
+		const uint32_t dst_bus = _dst_addr & 0xFF000000;
+		MDMA0::DstBusIsAHBTCM::write(dst_bus == 0x20000000 || dst_bus == 0);
 	}
 };
