889b1cf80ce021d162afe47998ec2ebc5deb931f
Dan Green <danngreen1@gmail.com>
2021-03-06 22:17:07 -0800

    Split PCA9685 non-dma driver and dma driver

--- drivers/pca9685_led_driver.cc
+++ drivers/pca9685_led_driver.cc
@@ -2,17 +2,15 @@
 
 namespace mdrivlib
 {
-PCA9685DmaDriver::PCA9685DmaDriver(I2CPeriph &i2c,
-								   uint32_t num_chips,
-								   const DMA_Config &dma_defs,
-								   PCA9685DmaDriver::FrameBuffer &frame_buf)
+
+PCA9685Driver::PCA9685Driver(I2CPeriph &i2c, uint32_t num_chips, uint32_t *frame_buf)
 	: i2c_periph_(i2c)
 	, num_chips_(num_chips)
-	, dma_(*this, dma_defs, frame_buf) {
+	, frame_buf_{frame_buf} {
 }
 
 // Reset chip registers
-LEDDriverError PCA9685DmaDriver::start() {
+LEDDriverError PCA9685Driver::start() {
 	uint8_t driverAddr;
 	LEDDriverError err;
 
@@ -28,24 +26,16 @@ LEDDriverError PCA9685DmaDriver::start() {
 
 // Enables I2C transfers in background using interrupts
 // (not implemented in I2CPeriph yet)
-void PCA9685DmaDriver::start_it_mode() {
+void PCA9685Driver::start_it_mode() {
 	start();
 	i2c_periph_.enable_IT(2, 2);
 }
 
-// Start transferring via DMA, given a frame buffer and the hardware
-// configuration
-LEDDriverError PCA9685DmaDriver::start_dma_mode() {
-	start_it_mode();
-	return dma_.start_dma();
-}
-
-LEDDriverError PCA9685DmaDriver::write_once(uint16_t chip_num) {
+LEDDriverError PCA9685Driver::write_chip(uint16_t chip_num) {
 
 	uint16_t driverAddr = I2C_BASE_ADDRESS | (chip_num << 1);
-
 	auto err = i2c_periph_.mem_write_IT(
-		driverAddr, REG_LED0, REGISTER_ADDR_SIZE, reinterpret_cast<uint8_t *>(dma_.frame_buffer), kNumLedsPerChip * 4);
+		driverAddr, REG_LED0, REGISTER_ADDR_SIZE, reinterpret_cast<uint8_t *>(frame_buf_), kNumLedsPerChip * 4);
 	if (err != I2CPeriph::I2C_NO_ERR)
 		return LEDDriverError::I2C_XMIT_TIMEOUT;
 
@@ -59,7 +49,7 @@ LEDDriverError PCA9685DmaDriver::write_once(uint16_t chip_num) {
 // led_element_number==15 is PWM pin 15 of PCA9685 chip with address 0
 // led_element_number==16 is PWM pin 0 of PCA9685 chip with address 1
 // ...
-LEDDriverError PCA9685DmaDriver::set_single_led(uint8_t led_element_number, uint16_t brightness) {
+LEDDriverError PCA9685Driver::set_single_led(uint8_t led_element_number, uint16_t brightness) {
 	uint8_t driver_addr;
 	uint8_t data[5];
 
@@ -95,7 +85,7 @@ LEDDriverError PCA9685DmaDriver::set_single_led(uint8_t led_element_number, uint
 // led_number==9 refers to PWM pins 12,13,14 of PCA9685 chip with address 1
 //
 // Note: This function cannot access pin 15
-LEDDriverError PCA9685DmaDriver::set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue) {
+LEDDriverError PCA9685Driver::set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue) {
 	uint8_t driverAddr;
 	uint8_t data[13];
 
@@ -131,7 +121,7 @@ LEDDriverError PCA9685DmaDriver::set_rgb_led(uint8_t led_number, uint16_t c_red,
 	return (err == I2CPeriph::Error::I2C_NO_ERR) ? LEDDriverError::None : LEDDriverError::I2C_XMIT_TIMEOUT;
 }
 
-LEDDriverError PCA9685DmaDriver::reset_chip(uint8_t driverAddr) {
+LEDDriverError PCA9685Driver::reset_chip(uint8_t driverAddr) {
 	LEDDriverError err;
 
 	err = write_register(driverAddr, REG_MODE1, 0b00000000); // clear sleep mode
@@ -147,15 +137,14 @@ LEDDriverError PCA9685DmaDriver::reset_chip(uint8_t driverAddr) {
 	err = write_register(driverAddr, REG_MODE1, 0b00100000); // auto increment
 	if (err != LEDDriverError::None)
 		return err;
-	err = write_register(driverAddr, REG_MODE2,
-						 0b00010001); // INVERT=1, OUTDRV=0, OUTNE=01
+	err = write_register(driverAddr, REG_MODE2, 0b00010001); // INVERT=1, OUTDRV=0, OUTNE=01
 	if (err != LEDDriverError::None)
 		return err;
 
 	return LEDDriverError::None;
 }
 
-LEDDriverError PCA9685DmaDriver::write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue) {
+LEDDriverError PCA9685Driver::write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue) {
 	uint8_t data[2] = {registerAddr, registerValue};
 
 	driverAddr = I2C_BASE_ADDRESS | (driverAddr << 1);
@@ -167,16 +156,12 @@ LEDDriverError PCA9685DmaDriver::write_register(uint8_t driverAddr, uint8_t regi
 
 // returns led element number of the red element of the given RGB LED id (green
 // is red + 1, blue = red + 2)
-uint8_t PCA9685DmaDriver::get_red_led_element_id(uint8_t rgb_led_id) {
+uint8_t PCA9685Driver::get_red_led_element_id(uint8_t rgb_led_id) {
 	return (rgb_led_id * 3) + (rgb_led_id / kNumRGBLEDsPerChip);
 }
 
-uint8_t PCA9685DmaDriver::get_chip_num(uint8_t rgb_led_id) {
+uint8_t PCA9685Driver::get_chip_num(uint8_t rgb_led_id) {
 	return (rgb_led_id / kNumRGBLEDsPerChip);
 }
 
-// returns the address within the frame buffer of the requested led and chip
-uint32_t *const PCA9685DmaDriver::get_buf_addr(const uint32_t chip_num, const uint32_t led_num) {
-	return &(dma_.frame_buffer[chip_num * kNumLedsPerChip + led_num]);
-}
 } // namespace mdrivlib
--- drivers/pca9685_led_driver.hh
+++ drivers/pca9685_led_driver.hh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "conf/led_driver_conf.hh"
-#include "dma_config_struct.hh"
 #include "hal_callback.hh"
 #include "i2c.hh"
 #include "led_driver.hh"
@@ -9,110 +8,43 @@
 
 namespace mdrivlib
 {
-// Todo: the DMa drivers inherits from non-dma driver? Or else remove all non-DMA commands from DMA drivr
-class PCA9685DmaDriver : public ILedDmaDriver {
+
+// Todo: finish this and use it
+class PCA9685Driver {
 public:
-	const static int kNumLedsPerChip = 16;
-	const static int kNumRGBLEDsPerChip = 5;
-	const static size_t kRequiredBufferSize = kNumLedDriverChips * PCA9685DmaDriver::kNumLedsPerChip;
+	constexpr static int kNumLedsPerChip = 16;
+	constexpr static int kNumRGBLEDsPerChip = 5;
 
-	using FrameBuffer = uint32_t[kRequiredBufferSize];
+	// returns the address within the frame buffer of the requested led and chip
+	static constexpr const uint32_t *get_buf_addr(const uint32_t *frame_buf, uint32_t chip_num, uint32_t led_num) {
+		return &(frame_buf[chip_num * kNumLedsPerChip + led_num]);
+	}
 
-public:
-	PCA9685DmaDriver(I2CPeriph &i2c, uint32_t num_chips, const DMA_Config &dma_defs, FrameBuffer &frame_buf);
+	PCA9685Driver(I2CPeriph &i2c, uint32_t num_chips, uint32_t *frame_buf);
 
-	virtual LEDDriverError start();
-	virtual void start_it_mode();
-	virtual LEDDriverError start_dma_mode();
-	virtual uint32_t *const get_buf_addr(const uint32_t chip_num, const uint32_t led_num);
-
+	LEDDriverError start();
+	void start_it_mode();
+	LEDDriverError start_dma_mode();
 	LEDDriverError set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue);
 	LEDDriverError set_single_led(uint8_t led_element_number, uint16_t brightness);
 	uint8_t get_red_led_element_id(uint8_t rgb_led_id);
 	uint8_t get_chip_num(uint8_t rgb_led_id);
+	LEDDriverError write_chip(uint16_t chip_num);
 
-private:
+protected:
 	I2CPeriph &i2c_periph_;
-	uint32_t num_chips_;
+	const uint32_t num_chips_;
 	LEDDriverError led_error_;
-
-	LEDDriverError write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue);
-	LEDDriverError reset_chip(uint8_t driverAddr);
-	LEDDriverError write_once(uint16_t chip_num);
-
-	// Todo: pull this out of PCA driver, it should be a template class so we can use other LedDmaDrivers
-	class DMADriver {
-		friend class PCA9685DmaDriver;
-
-	public:
-		DMADriver(PCA9685DmaDriver &parent, const DMA_Config &dma_defs, FrameBuffer &frame_buf);
-		LEDDriverError start_dma();
-
-	private:
-		LEDDriverError init_dma();
-		void advance_frame_buffer();
-		void write_current_frame_to_chip();
-
-		PCA9685DmaDriver &driver_;
-		DMA_HandleTypeDef hal_dma_h_;
-		Interrupt dma_task;
-		uint8_t cur_chip_num_ = 0;
-		uint32_t *frame_buffer_cur_pos;
-		uint32_t (&frame_buffer)[kRequiredBufferSize];
-		const DMA_Config &dma_defs_;
-		bool transfer_complete;
-		// Todo: Fix HALCallbacks so we can move a temporary (stack-created) lambda into HALCBs
-		HALCallback transfer_complete_cb{HALCallbackID::I2C_MemTxCplt, [this]() {
-											 advance_frame_buffer();
-											 if (!dma_defs_.continuous && cur_chip_num_ == 0)
-												 transfer_complete = true;
-											 else
-												 write_current_frame_to_chip();
-										 }};
-	};
-	friend class PCA9685DmaDriver::DMADriver;
-	DMADriver dma_;
-
-private:
-	const static inline uint32_t REG_MODE1 = 0x00;
-	const static inline uint32_t REG_MODE2 = 0x01;
-	const static inline uint32_t REG_LED0 = 0x06;
-	const static inline uint32_t I2C_BASE_ADDRESS = 0b10000000;
-	const static inline auto REGISTER_ADDR_SIZE = I2C_MEMADD_SIZE_8BIT;
-};
-
-class PCA9685Driver : public ILedDmaDriver {
-	const static int kNumLedsPerChip = 16;
-	const static int kNumRGBLEDsPerChip = 5;
-	const static size_t kRequiredBufferSize = kNumLedDriverChips * PCA9685DmaDriver::kNumLedsPerChip;
-
-	using FrameBuffer = uint32_t[kRequiredBufferSize];
-
-public:
-	PCA9685Driver(I2CPeriph &i2c, uint32_t num_chips, FrameBuffer &frame_buf);
-
-	virtual LEDDriverError start();
-	virtual void start_it_mode();
-	virtual LEDDriverError start_dma_mode();
-	virtual uint32_t *const get_buf_addr(const uint32_t chip_num, const uint32_t led_num);
-
-	LEDDriverError set_rgb_led(uint8_t led_number, uint16_t c_red, uint16_t c_green, uint16_t c_blue);
-	LEDDriverError set_single_led(uint8_t led_element_number, uint16_t brightness);
-	uint8_t get_red_led_element_id(uint8_t rgb_led_id);
-	uint8_t get_chip_num(uint8_t rgb_led_id);
-
-private:
-	I2CPeriph &i2c_periph_;
-	uint32_t num_chips_;
-	LEDDriverError led_error_;
+	uint32_t *frame_buf_;
 
 	LEDDriverError write_register(uint8_t driverAddr, uint8_t registerAddr, uint8_t registerValue);
 	LEDDriverError reset_chip(uint8_t driverAddr);
 
-	const static inline uint32_t REG_MODE1 = 0x00;
-	const static inline uint32_t REG_MODE2 = 0x01;
-	const static inline uint32_t REG_LED0 = 0x06;
-	const static inline uint32_t I2C_BASE_ADDRESS = 0b10000000;
-	const static inline auto REGISTER_ADDR_SIZE = I2C_MEMADD_SIZE_8BIT;
+	constexpr static inline uint32_t REG_MODE1 = 0x00;
+	constexpr static inline uint32_t REG_MODE2 = 0x01;
+	constexpr static inline uint32_t REG_LED0 = 0x06;
+	constexpr static inline uint32_t I2C_BASE_ADDRESS = 0b10000000;
+	constexpr static inline auto REGISTER_ADDR_SIZE = I2C_MEMADD_SIZE_8BIT;
 };
+
 } // namespace mdrivlib
