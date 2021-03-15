8da36d2c49dcf9f1ddc5d34e555339fc4323597a
Dan Green <danngreen1@gmail.com>
2021-03-15 10:51:09 -0700

    Testing BDMA for SPI

--- drivers/spi_screen_driver.hh
+++ drivers/spi_screen_driver.hh
@@ -7,58 +7,6 @@
 #include "system.hh"
 #include "util/math.hh"
 
-template<typename ConfT>
-struct SpiScreenDriver2 {
-	SpiScreenDriver2() {
-		spi.disable();
-		spi.configure();
-
-		spi.set_tx_message_size(1);
-		spi.enable();
-	}
-
-private:
-	SpiPeriph<typename ConfT::ScreenSpiConf> spi;
-	typename ConfT::DCPin dcpin;
-
-	void wait_until_ready() {
-		while (!spi.is_tx_complete())
-			;
-	}
-
-protected:
-	// Blocking Mode:
-	enum PacketType { Cmd, Data };
-
-	template<PacketType MessageType>
-	void transmit(uint8_t byte) {
-		spi.disable();
-		spi.set_tx_message_size(1);
-		spi.enable();
-		if constexpr (MessageType == Cmd)
-			dcpin.low();
-		if constexpr (MessageType == Data)
-			dcpin.high();
-		spi.load_tx_data(byte);
-		spi.start_transfer();
-		wait_until_ready();
-		// spi.clear_EOT_flag();
-		spi.clear_TXTF_flag();
-	}
-
-	void transmit_data_32(uint16_t halfword1, uint16_t halfword2) {
-		spi.disable();
-		spi.set_tx_message_size(4);
-		spi.enable();
-		dcpin.high();
-		spi.load_tx_data(MathTools::swap_bytes_and_combine(halfword2, halfword1));
-		spi.start_transfer();
-		wait_until_ready();
-		// spi.clear_EOT_flag();
-		spi.clear_TXTF_flag();
-	}
-};
-
 // Todo: this is not necessarily an SPI screen driver, it's a general SPI driver that has a hi/low pin for each transfer
 // and it's blocking mode only
 template<typename ConfT>
@@ -242,10 +190,69 @@ struct DmaSpiScreenDriver {
 		spi.clear_TXTF_flag();
 	}
 
-	void start_dma_transfer(uint32_t src, uint32_t sz, std::function<void(void)> &&cb) {
+	void init_bdma(std::function<void(void)> &&cb) {
 		callback = std::move(cb);
-		mdma.registerCallback([&]() { callback(); });
-		start_dma_transfer(src, sz);
+		target::RCC_Control::BDMA_P::set();
+
+		BDMA_Channel_TypeDef *stream;
+		if constexpr (ConfT::BDMAConf::StreamNum == 2)
+			stream = BDMA_Channel2;
+
+		hdma_spi6_tx.Instance = stream;
+		hdma_spi6_tx.Init.Request = ConfT::BDMAConf::RequestNum;
+		hdma_spi6_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+		hdma_spi6_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+		hdma_spi6_tx.Init.MemInc = DMA_MINC_ENABLE;
+		hdma_spi6_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+		hdma_spi6_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+		hdma_spi6_tx.Init.Mode = DMA_NORMAL;
+		hdma_spi6_tx.Init.Priority = DMA_PRIORITY_LOW;
+		if (HAL_DMA_Init(&hdma_spi6_tx) != HAL_OK) {
+			__BKPT();
+		}
+
+		// This wasn't used:
+		dma_tc_flag_index = dma_get_TC_flag_index(stream);
+		dma_te_flag_index = dma_get_TE_flag_index(stream);
+		dma_isr_reg = dma_get_ISR_reg(stream);
+		dma_ifcr_reg = dma_get_IFCR_reg(stream);
+		InterruptManager::registerISR(ConfT::BDMAConf::IRQn, ConfT::BDMAConf::pri, ConfT::BDMAConf::subpri, [&]() {
+			if (*dma_isr_reg & dma_tc_flag_index) {
+				*dma_ifcr_reg = dma_tc_flag_index;
+				callback();
+			}
+			if (*dma_isr_reg & dma_te_flag_index) {
+				*dma_ifcr_reg = dma_te_flag_index;
+				// Error: debug breakpoint or logging here
+			}
+		});
+		HAL_NVIC_DisableIRQ(ConfT::BDMAConf::IRQn);
+	}
+
+	void start_bdma_transfer(uint32_t src, uint32_t sz) {
+		spi.disable();
+		HAL_NVIC_EnableIRQ(ConfT::BDMAConf::IRQn);
+		uint32_t dst = spi.get_tx_datareg_addr();
+		Debug::Pin1::high();
+		HAL_DMA_Start_IT(&hdma_spi6_tx, src, dst, sz);
+
+		if (sz <= 0xFFFF) {
+			spi.set_tx_message_size(sz);
+			spi.set_tx_message_reload_size(0);
+		} else if (sz <= (0xFFFF * 2)) {
+			spi.set_tx_message_size(0xFFFF);
+			spi.set_tx_message_reload_size(sz - 0xFFFF);
+		} else
+			return; // Todo: handle transfers > 128k packets by setting up an ISR on TSERF that sets the reload size
+		// spi.set_tx_message_size(8);
+		// spi.set_tx_message_reload_size(0);
+
+		dcpin.high();
+		spi.clear_EOT_flag();
+		spi.clear_TXTF_flag();
+		spi.enable_tx_dma();
+		spi.enable();
+		spi.start_transfer();
 	}
 
 	void init_mdma(std::function<void(void)> &&cb) {
@@ -253,19 +260,16 @@ struct DmaSpiScreenDriver {
 		mdma.registerCallback([&]() { callback(); });
 	}
 
-	//@50MHz SCLK:
-	// 1.45ms is xferred (for waht should be half a frame)
-	// 1bit  = 0.020us
-	// 1byte = 0.160us
-	// half-frame = 240*240*2 / 2 =57600B = 9.216ms @ 50MHz
-	// and it does look like 1.45/9.216 = 15.7% of the screen is drawn (with garbage)
+	void start_dma_transfer(uint32_t src, uint32_t sz, std::function<void(void)> &&cb) {
+		callback = std::move(cb);
+		mdma.registerCallback([&]() { callback(); });
+		start_dma_transfer(src, sz);
+	}
+
 	void start_dma_transfer(uint32_t src, uint32_t sz) {
 		spi.disable();
 		uint32_t dst = spi.get_tx_datareg_addr();
 
-		// sz is indeed 57600
-		// dst is indeed TXDR
-		// src is indeed 0x24000000
 		mdma.config_transfer((void *)dst, (void *)src, sz);
 
 		if (sz <= 0xFFFF) {
@@ -276,6 +280,8 @@ struct DmaSpiScreenDriver {
 			spi.set_tx_message_reload_size(sz - 0xFFFF);
 		} else
 			return; // Todo: handle transfers > 128k packets by setting up an ISR on TSERF that sets the reload size
+		// spi.set_tx_message_size(8);
+		// spi.set_tx_message_reload_size(0);
 
 		dcpin.high();
 		spi.clear_EOT_flag();
@@ -286,30 +292,29 @@ struct DmaSpiScreenDriver {
 		mdma.start_transfer();
 	}
 
-private:
-	void init_txc_interrupt() {
-		InterruptManager::registerISR(
-			ConfT::ScreenSpiConf::IRQn, ConfT::ScreenSpiConf::priority1, ConfT::ScreenSpiConf::priority2, [this]() {
-				if (spi.is_end_of_transfer()) {
-					spi.clear_EOT_flag();
-					spi.clear_TXTF_flag();
-					_ready = true;
-				}
-			});
-		NVIC_EnableIRQ(ConfT::ScreenSpiConf::IRQn);
+	void wait_until_ready() {
+		while (!spi.is_tx_complete())
+			;
 	}
+
+private:
+	// void init_txc_interrupt() {
+	// 	InterruptManager::registerISR(
+	// 		ConfT::ScreenSpiConf::IRQn, ConfT::ScreenSpiConf::priority1, ConfT::ScreenSpiConf::priority2, [this]() {
+	// 			if (spi.is_end_of_transfer()) {
+	// 				spi.clear_EOT_flag();
+	// 				spi.clear_TXTF_flag();
+	// 				_ready = true;
+	// 			}
+	// 		});
+	// 	NVIC_EnableIRQ(ConfT::ScreenSpiConf::IRQn);
+	// }
 
 private:
 	SpiPeriph<typename ConfT::ScreenSpiConf> spi;
 	typename ConfT::DCPin dcpin;
 	std::function<void(void)> callback;
 
-	bool _ready = true;
-	void wait_until_ready() {
-		while (!_ready) {
-		}
-		_ready = false;
-	}
 	MemoryTransfer mdma;
 
 	volatile uint32_t *dma_ifcr_reg;
@@ -319,39 +324,3 @@ private:
 
 	DMA_HandleTypeDef hdma_spi6_tx;
 };
-
-// void init_bdma() {
-// BDMA_Channel_TypeDef *stream;
-// if constexpr (ConfT::BDMAConf::StreamNum == 2)
-// 	stream = BDMA_Channel2;
-
-// hdma_spi6_tx.Instance = stream;
-// hdma_spi6_tx.Init.Request = ConfT::BDMAConf::RequestNum;
-// hdma_spi6_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-// hdma_spi6_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-// hdma_spi6_tx.Init.MemInc = DMA_MINC_ENABLE;
-// hdma_spi6_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-// hdma_spi6_tx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
-// hdma_spi6_tx.Init.Mode = DMA_NORMAL;
-// hdma_spi6_tx.Init.Priority = DMA_PRIORITY_LOW;
-// if (HAL_DMA_Init(&hdma_spi6_tx) != HAL_OK) {
-// 	__BKPT();
-// }
-
-// dma_tc_flag_index = dma_get_TC_flag_index(stream);
-// dma_te_flag_index = dma_get_TE_flag_index(stream);
-// dma_isr_reg = dma_get_ISR_reg(stream);
-// dma_ifcr_reg = dma_get_IFCR_reg(stream);
-
-// This wasn't used:
-// InterruptManager::registerISR(ConfT::BDMAConf::IRQn, ConfT::BDMAConf::pri, ConfT::BDMAConf::subpri, [&]() {
-// 	if (*dma_isr_reg & dma_tc_flag_index) {
-// 		*dma_ifcr_reg = dma_tc_flag_index;
-// 	}
-// 	if (*dma_isr_reg & dma_te_flag_index) {
-// 		*dma_ifcr_reg = dma_te_flag_index;
-// 		// Error: debug breakpoint or logging here
-// 	}
-// });
-// HAL_NVIC_DisableIRQ(ConfT::BDMAConf::IRQn);
-// }
