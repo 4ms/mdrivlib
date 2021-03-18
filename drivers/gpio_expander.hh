eeb965e6650b900613143e347d783fc5d43c5d1d
Dan Green <danngreen1@gmail.com>
2021-03-18 11:40:42 -0700

    GPIO Expander can read pins

--- drivers/gpio_expander.hh
+++ drivers/gpio_expander.hh
@@ -10,12 +10,32 @@ struct GPIO_expander_conf {
 	uint8_t addr;
 	Pin int_pin;
 	IRQType IRQn;
+	uint16_t config;
+
 	enum PinDirection { Output = 0, Input = 1 };
-	PinDirection config[16];
+	static constexpr uint16_t AllInputs() {
+		return 0xFFFF;
+	}
+	static constexpr uint16_t AllOutputs() {
+		return 0x0000;
+	}
+	static constexpr uint16_t Config(PinDirection pindirs[16]) {
+		uint16_t c = 0;
+		for (unsigned i = 0; i < 16; i++)
+			c |= (pindirs[i] == Input) ? (1 << i) : 0;
+		return c;
+	}
 };
 
 struct GPIOExpander {
 	static constexpr uint8_t NumChans = 16;
+	enum Error {
+		None,
+		WriteConfigFailed,
+		InvalidChannelID,
+		ReadFailed,
+		WriteFailed,
+	};
 
 	GPIOExpander(I2CPeriph &i2c, const GPIO_expander_conf &conf)
 		: _conf{conf}
@@ -23,35 +43,52 @@ struct GPIOExpander {
 		, _i2c{i2c} {
 	}
 
-	void init() {
+	auto start() {
+		_data[0] = ConfigPort0;
+		_data[1] = static_cast<uint8_t>(_conf.config & 0x00FF);
+		_data[2] = static_cast<uint8_t>(_conf.config >> 8);
+		auto err = _i2c.write(_device_addr, _data, 3);
+		if (err != I2CPeriph::I2C_NO_ERR)
+			return Error::WriteConfigFailed;
+		return Error::None;
+	}
+
+	// Todo:
+	// Create an EXTI interrupt to respond to chip telling us there's a pin change
+	// void start_ISR(std::function<void(void)> &&cb) {
+	// 	callback = std::move(cb);
+	// 	InterruptManager::registerISR(_conf.IRQn, _conf.pri1, _conf.pri2, [&]() { callback(); });
+	// }
+
+	auto prepare_read() {
 		_data[0] = InputPort0;
-		_i2c.write(_device_addr, _data, 1);
+		auto err = _i2c.write_IT(_device_addr, _data, 1);
+		return err == I2CPeriph::I2C_NO_ERR ? Error::None : Error::WriteFailed;
 	}
 
-	void start_ISR() {
-		// Create an EXTI interrupt to respond to chip telling us there's a pin change
-		// InterruptManager::registerISR(_conf.IRQn, [this]() { read_pins(); });
-	}
-	void read_pins() {
-		// write InputPort0 or 1
-		// read InputPort 0 or 1
+	auto read_pins() {
+		auto err = _i2c.read_IT(_device_addr, _data, 2);
+		return err == I2CPeriph::I2C_NO_ERR ? Error::None : Error::ReadFailed;
 	}
 
-	void set_output_values(uint8_t port, uint8_t mask) {
-		// write OutputPort 0 or 1 with mask
+	auto set_output_values(uint16_t mask) {
+		// Todo: test this on hardware
+		_data[0] = OutputPort0;
+		_data[1] = mask & 0x00FF;
+		_data[2] = mask >> 8;
+		auto err = _i2c.write_IT(_device_addr, _data, 3);
+		return err == I2CPeriph::I2C_NO_ERR ? Error::None : Error::WriteFailed;
 	}
 
 	uint16_t collect_last_reading() {
-		uint16_t clear_mask = _cur_port ? 0x00FF : 0xFF00;
-		last_reading &= clear_mask;
-		last_reading |= _data[0] << (_cur_port * 8);
+		last_reading = _data[1] << 8 | _data[0];
 		return last_reading;
 	}
 
-	bool get_val(unsigned chan) {
-		if (chan >= NumChans)
+	bool get_pin_state(unsigned pinnum) {
+		if (pinnum >= NumChans)
 			return false;
-		return last_reading & (1 << chan) ? true : false;
+		return last_reading & (1 << pinnum) ? true : false;
 	}
 
 private:
@@ -59,7 +96,8 @@ private:
 	const uint8_t _device_addr;
 	I2CPeriph &_i2c;
 	uint16_t last_reading;
-	uint8_t _data[2];
+	uint8_t _data[3];
+	uint16_t _data16;
 	unsigned _cur_port = 0;
 
 	enum Registers : uint8_t {
@@ -76,9 +114,4 @@ private:
 	static constexpr uint8_t Input = 1;
 };
 
-const GPIO_expander_conf gpio_expander_conf = {
-	.addr = 0b0100000, // un-shifted address
-	.int_pin = {GPIO::D, 11, PinMode::Input},
-	.IRQn = EXTI15_10_IRQn,
-};
 } // namespace mdrivlib
