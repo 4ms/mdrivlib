deb97a5741cfac0ecdcb9cc747670f0f1bee5a1f
Dan Green <danngreen1@gmail.com>
2021-04-02 10:13:08 -0700

    BDMA explicitly captures callback

--- drivers/bdma.hh
+++ drivers/bdma.hh
@@ -75,13 +75,13 @@ struct BDMATransfer {
 	template<typename CallbackT>
 	void register_callback(CallbackT &&callback) {
 		HAL_NVIC_DisableIRQ(ConfT::IRQn);
-		InterruptManager::registerISR(ConfT::IRQn, ConfT::pri, ConfT::subpri, [=]() {
+		InterruptManager::registerISR(ConfT::IRQn, ConfT::pri, ConfT::subpri, [callback, this]() {
 			if (*dma_isr_reg & dma_tc_flag_index) {
 				*dma_ifcr_reg = dma_tc_flag_index;
 				if constexpr (ConfT::half_transfer_interrupt_enable)
 					callback(1);
 				else
-				callback();
+					callback();
 			}
 			if constexpr (ConfT::half_transfer_interrupt_enable) {
 				if (*dma_isr_reg & dma_ht_flag_index) {
