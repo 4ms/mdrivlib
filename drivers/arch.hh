35d5df0749d1fee55c588e15c8d3105915e63d99
Dan Green <danngreen1@gmail.com>
2021-04-12 17:08:46 -0700

    Adding stm32mp1 target. Split rcc.hh into multiple #includes

--- drivers/arch.hh
+++ drivers/arch.hh
@@ -1,27 +1,61 @@
 #pragma once
 
+namespace mdrivlib
+{
+enum class SupportedTargets {
+	stm32mp1,
+	stm32h7x5,
+	stm32f7,
+	stm32f4,
+};
+}
+
+//
+// STM32MP1
+//
+#if defined(STM32MP1)
+namespace mdrivlib
+{
+namespace stm32mp1
+{
+constexpr bool TARGET_FOUND = true;
+constexpr auto TYPE = SupportedTargets::stm32mp1;
+} // namespace stm32mp1
+} // namespace mdrivlib
+namespace target = mdrivlib::stm32mp1;
+#endif
+
+//
+// STM32H745 and STM32H755
+//
 #if defined(STM32H745xx) || defined(STM32H755xx)
+#ifndef STM32H7x5
+#define STM32H7x5
+#endif
 namespace mdrivlib
 {
 namespace stm32h7x5
 {
-//
+constexpr bool TARGET_FOUND = true;
+constexpr auto TYPE = SupportedTargets::stm32mp1;
 } // namespace stm32h7x5
 } // namespace mdrivlib
-
 namespace target = mdrivlib::stm32h7x5;
 #endif
 
+//
+// STM32F746
+//
 #if defined(STM32F746xx)
-
 namespace mdrivlib
 {
 namespace stm32f7xx
 {
-//
+constexpr bool TARGET_FOUND = true;
+constexpr auto TYPE = SupportedTargets::stm32mp1;
 } // namespace stm32f7xx
 } // namespace mdrivlib
-
 namespace target = mdrivlib::stm32f7xx;
 #endif
 
+static_assert(target::TARGET_FOUND == true, "No target specified");
---
+++ drivers/target/stm32f7xx/rcc.hh
@@ -0,0 +1,15 @@
+#pragma once
+#include "register_access.hh"
+#include "stm32xx.h"
+
+namespace mdrivlib
+{
+namespace stm32f7xx
+{
+namespace RCC_Control
+{
+using RegisterDataT = uint32_t;
+}
+} // namespace stm32f7xx
+}
+
