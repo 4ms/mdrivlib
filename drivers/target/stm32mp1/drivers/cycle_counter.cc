4050196be481c82caca6b94c0e7fd7e8538caa85
Dan Green <danngreen1@gmail.com>
2021-04-16 15:53:13 -0700

    Put cycle_counter implementation in its own file target-specific

---
+++ drivers/target/stm32mp1/drivers/cycle_counter.cc
@@ -0,0 +1,17 @@
+#include "drivers/cycle_counter.hh"
+
+namespace mdrivlib
+{
+void CycleCounter::init() {
+	// if (!(DWT->CTRL & DWT_CTRL_NOCYCCNT_Msk)) {
+	// 	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	// 	DWT->LAR = 0xC5ACCE55;
+	// 	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+	// }
+}
+
+uint32_t CycleCounter::read_cycle_count() {
+	// return DWT->CYCCNT;
+	return 1;
+}
+} // namespace mdrivlib
