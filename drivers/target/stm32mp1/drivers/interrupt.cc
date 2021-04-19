1df200ff20d048850f1bbf9979c4a8ee98cb922e
Dan Green <danngreen1@gmail.com>
2021-04-19 12:19:28 -0700

    Made interrupt.cc target-specific.
    Existing file ==> stm32h7x5 (could be generic NVIC interrupt system)
    new file ==> stm32mp1 (could be generic GIC interrupt system)

---
+++ drivers/target/stm32mp1/drivers/interrupt.cc
@@ -0,0 +1,22 @@
+#include "interrupt.hh"
+
+static void ISRHandler(unsigned irqnum) {
+	InterruptManager::callISR(irqnum);
+}
+
+extern "C" {
+
+void __attribute__((interrupt)) SVC_Handler() {
+	__BKPT();
+}
+
+void __attribute__((interrupt)) IRQ_Handler() {
+	IRQn_ID_t irqn = IRQ_GetActiveIRQ();
+	ISRHandler(irqn);
+	IRQ_EndOfInterrupt(irqn);
+}
+
+void __attribute__((interrupt)) FIQ_Handler() {
+	__BKPT();
+}
+}
