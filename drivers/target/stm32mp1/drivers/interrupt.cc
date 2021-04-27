befbe004ae0f3b14be497eadb802637e4e8a90c7
Dan Green <danngreen1@gmail.com>
2021-04-27 16:55:01 -0700

    CA7: IRQ Handler is re-entrant, allows for multiple interrupts

--- drivers/target/stm32mp1/drivers/interrupt.cc
+++ drivers/target/stm32mp1/drivers/interrupt.cc
@@ -1,4 +1,4 @@
-#include "interrupt.hh"
+#include "drivers/interrupt.hh"
 
 static void ISRHandler(unsigned irqnum) {
 	InterruptManager::callISR(irqnum);
@@ -11,10 +11,29 @@ void __attribute__((interrupt)) SVC_Handler() {
 }
 
 void __attribute__((interrupt)) IRQ_Handler() {
+	asm volatile(""
+				 "SUB lr, lr, #4 	\n"
+				 "SRSFD sp!, #0x1f 	\n" // use SRS to save LR_irq and SPSR_irq in one step onto the System mode stack
+				 "CPS #0x1f 		\n" // Use CPS to switch to system mode"
+				 "PUSH {r0-r3, r12} \n" // Store remaining AAPCS registers on the System mode stack
+				 "AND r1, sp, #4  	\n" // Ensure stack is 8-byte aligned. Store adjustment and
+										// LR_sys to stack
+				 "SUB sp, sp, r1 	\n"
+				 "PUSH {r1, lr} 	\n");
 	IRQn_ID_t irqn = IRQ_GetActiveIRQ();
-	if (irqn < 255)
+	__enable_irq();
+	if (irqn < 255) {
 		ISRHandler(irqn);
+	}
 	IRQ_EndOfInterrupt(irqn);
+	asm volatile("CPSID i 			\n"	  // Disable IRQ with CPS
+				 "POP {r1, lr} 		\n"	  // Restore LS_sys
+				 "ADD sp, sp, r1  	\n"	  // Unadjust stack
+				 "POP {r0-r3, r12}  \n"	  // Restore AAPCS registers
+				 "RFEFD sp! 		\n"); // REturn using RFE from the System mode stack
+}
+
+void C_irq_handler() {
 }
 
 void __attribute__((interrupt)) FIQ_Handler() {
