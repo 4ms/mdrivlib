d0cd244e1d2dbf39957514517e8a67c185ef5b65
Dan Green <danngreen1@gmail.com>
2021-04-28 16:55:50 -0700

    WIP: Nested IRQ Handler in asm

--- drivers/startup_ca7.s
+++ drivers/startup_ca7.s
@@ -83,14 +83,20 @@ irq_loop:
 
     /* Supervisor mode */
     msr cpsr_c, MODE_SVC
-    ldr r1, =_stack_start
-    ldr sp, =_stack_end
+    ldr r1, =_svc_stack_start
+    ldr sp, =_svc_stack_end
 
-stack_loop:
+svc_loop:
     cmp r1, sp
     strlt r0, [r1], #4
-    blt stack_loop
+    blt svc_loop
 
+	// Initialize nested ISR count
+	ldr r0, =0
+	ldr r2, =_svc_nested_isr_count
+	str r0, [r2]
+
+	/* USER and SYS mode */
 	msr cpsr_c, MODE_SYS
 	ldr sp, =_user_stack_end
 
@@ -143,4 +149,64 @@ PAbt_Handler:
 
 DAbt_Handler:
 	b .
+
+IRQ_Handler:
+	sub lr, lr, #4
+
+	//SRSDB sp!, MODE_SYS 	// Save LR_irq and SPSR_irq onto the System mode stack, and update SP_sys
+	srsdb MODE_SYS 		// ??ZZ: Save LR_irq and SPSR_irq onto the System mode stack 
+
+	cps MODE_SYS 		 	// Switch to system mode
+
+	//PUSH {r0-r3, r12} 	// Store remaining AAPCS registers on the System mode stack
+	push {r0-r3, r12, lr} 	// ZZ: Store remaining AAPCS registers on the System mode stack
+
+	//
+	cps MODE_SVC 			// ZZ: Use SVC mode stack instead of USR/SYS, so ISR has plenty of stack available
+
+	and r3, sp, #4  	 	// Ensure stack is 8-byte aligned. 
+	sub sp, sp, r3 
+
+	//push {r1, lr}  		// Store adjustment and LR_sys to SYS stack 
+	push {r2, r3}  			// ZZ: Store adjustment and r2(why?) to SVC stack
+
+	ldr r2, =_svc_nested_isr_count
+	ldr r0, [r2]
+	add r0, r0, #1
+	str r0, [r2]
+
+	bl IRQ_GetActiveIRQ
+
+	push {r0, r1} 			//ZZ: Why?	
+	//lsl r0, r0, #3
+
+	cpsie i 				// Enable interrupts
+
+	bl ISRHandler
+	pop {r0, r1}
+	bl IRQ_EndOfInterrupt
+
+	//Pre-empt here
+
+	cpsid i 				// Disable interrupts while we exit
+
+							// ZZ: Decrement nesting count
+	ldr r2, =_svc_nested_isr_count
+	ldr r0, [r2]
+	sub r0, r0, #1
+	str r0, [r2]
+
+
+	//pop {r1, lr} 			// Restore LS_sys 
+	// add sp, sp, r1 		// Unadjust stack
+
+	pop {r2, r3} 			// ZZ: Restore previous stack pointer
+	add sp, sp, r3  		// ZZ: Restore previous stack pointer
+
+	cps #MODE_SYS
+	pop {r0-r3, r12, lr}
+	rfeia sp!
+	
+
+
 
