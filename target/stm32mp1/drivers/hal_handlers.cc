95f0be28a8ca8ac2d11575ea6d48390e327a2214
Dan Green <danngreen1@gmail.com>
2021-04-29 14:14:32 -0700

    Reverting HAL_GetTick to use ints (WIP: debugging nested ISRs)

--- target/stm32mp1/drivers/hal_handlers.cc
+++ target/stm32mp1/drivers/hal_handlers.cc
@@ -3,6 +3,7 @@
 extern "C" {
 uint32_t HAL_GetTick(void) {
 	// floats divide faster than ints
-	return (uint32_t)((float)PL1_GetCurrentPhysicalValue() / ((float)HSE_VALUE / 1000.f));
+	// return (uint32_t)((float)PL1_GetCurrentPhysicalValue() / ((float)HSE_VALUE / 1000.f));
+	return (PL1_GetCurrentPhysicalValue() / (HSE_VALUE / 1000));
 }
 }
