876c8daf48cc66f80fb89858dda925abf1143700
Dan Green <danngreen1@gmail.com>
2021-04-29 12:11:44 -0700

    Fixed HAL_Delay timer (off by 2x)

--- target/stm32mp1/drivers/hal_handlers.cc
+++ target/stm32mp1/drivers/hal_handlers.cc
@@ -2,6 +2,7 @@
 
 extern "C" {
 uint32_t HAL_GetTick(void) {
-	return (PL1_GetCurrentPhysicalValue() / (HSE_VALUE / 2000)); // FIXME: Why is this off by 1?
+	// floats divide faster than ints
+	return (uint32_t)((float)PL1_GetCurrentPhysicalValue() / ((float)HSE_VALUE / 1000.f));
 }
 }
