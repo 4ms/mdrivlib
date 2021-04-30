d9b6d0db1b2ddf4667ee59030b95dfafd8ac1575
Dan Green <danngreen1@gmail.com>
2021-04-30 13:24:12 -0700

    Simplified IRQ_EndOfInterrupt with inline asm

--- target/stm32mp1/drivers/interrupt.cc
+++ target/stm32mp1/drivers/interrupt.cc
@@ -14,8 +14,6 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		".equ MODE_SYS, 0x1F  			\n"
 		".equ GICCPU_BASE_low, 0x2000 	\n"
 		".equ GICCPU_BASE_high, 0xA002 	\n"
-		// ".equ GICC_HPPIR_offset, 24   	\n"
-		// ".equ GICC_IAR_offset, 12		\n"
 		"sub lr, lr, #4 				\n" // LR = addr of *next* instr to execute; subtract 4 so we return to instr
 											// that was about to be executed
 		"srsdb sp!, MODE_SYS 			\n" // Save LR_irq and SPSR_irq onto the System mode stack, (decrement SP_sys)
@@ -25,19 +23,29 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		"sub sp, sp, r3  				\n" //
 		"push {r3}  					\n" // Store adjustment to stack
 
-		"mov r3, #GICCPU_BASE_low		\n" // (this block used to be 'bl IRQ_GetActiveIRQ'
+		// The following block used to be 'bl IRQ_GetActiveIRQ', which contained an Errata fix
+		"mov r3, #GICCPU_BASE_low		\n"
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
 		"ldr r2, [r3, #24] 				\n" // Get Highest Pending Interrupt number
 		"ldr r2, [r3, #12]				\n" // Acknowledge it with a read to the Interrupt Acknowledge Register
-		"uxth r0, r2 					\n" // Unpack it... why?
-		"dsb sy							\n" // Data barrier (why?)
+		"ubfx r0, r2, #0, #10 			\n" // Mask IRQ number to 0..1023
+		// "uxth r0, r2 					\n" // Grab just the bottom 16bits
+		"dsb sy							\n" // Data barrier (Todo: research why)
 
-		"push {r0, r1} 					\n" // Push IRQ number (r0) and ??? (r1) so ISRHandler doesn't overwrite them
-											// (we need them for IRQ_EndOfInterrupt )
+		"push {r0} 						\n" // Push IRQ number (r0) so ISRHandler doesn't overwrite
 		"cpsie i 						\n" // Enable interrupts
 		"bl ISRHandler 					\n" // Handle the ISR
-		"pop {r0, r1} 					\n" // Restore the IRQ number
-		"bl IRQ_EndOfInterrupt  		\n" // Signal the GIC that we're done processing
+		"pop {r0} 						\n" // Restore the IRQ number
+
+		// The following block was 'bl IRQ_EndOfInterrupt', which contained an Errata fix
+		"ubfx r2, r0, #0, #10 			\n" // Mask IRQ number to 0..1023
+		"cmp r2, #0x03fc 				\n" // Check if it's a valid IRQ number
+		"bge InvalidIRQNum 				\n" // Skip this if it's invalid
+		"mov r3, #GICCPU_BASE_low		\n" //
+		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
+		"str r2, [r3, #16] 				\n" // Write IRQ num to End of Interrupt Register to tell GIC we're done
+		"InvalidIRQNum: 				\n"
+
 		"cpsid i 						\n" // Disable interrupts while we exit
 		"pop {r3} 						\n" // Pop the stack adjustment
 		"add sp, sp, r3  				\n" // Restore previous stack pointer
