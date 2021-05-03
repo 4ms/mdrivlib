cd72f7362a099c46578d9c9fc54fb502ee422f51
Dan Green <danngreen1@gmail.com>
2021-05-03 14:14:09 -0700

    Cortex-A7 IRQs are Edge, so they can be de-asserted on reboot

--- target/stm32mp1/drivers/system.hh
+++ target/stm32mp1/drivers/system.hh
@@ -15,7 +15,15 @@ struct System {
 	static void SetVectorTable(uint32_t reset_address) {
 	}
 
-	// ARM CA7 uses 5-bit priority: pri1 is upper 3 bits (0-7), pri2 is lower 2 bits (0-3)
+	// Binary Point is set to 4 in IRQ_Initialize().
+	// From Table 3-2 in arm_gic_architecture_specifiation, the group and sub-group are:
+	//     ggg.sssss
+	// ARM CA7 uses 5-bit priority, and is left-aligned, so bottom 3 sss bits are ignored.
+	// So we have:
+	//     ggg.ssXXX
+	//
+	// So, pri1 is upper 3 bits (value can be 0-7), pri2 is lower 2 bits (value can be 0-3)
+	//
 	static void set_irq_priority(IRQn_Type irqn, uint32_t pri1, uint32_t pri2) {
 		auto pri = ((pri1 & 0b111) << 5) | (pri2 & 0b11 << 3);
 		GIC_SetPriority(irqn, pri);
@@ -27,7 +35,7 @@ struct System {
 
 	static void enable_irq(IRQn_Type irqn) {
 		auto status =
-			IRQ_SetMode((IRQn_ID_t)irqn, IRQ_MODE_TRIG_LEVEL_HIGH | IRQ_MODE_CPU_0 /*ALL*/ | IRQ_MODE_TYPE_IRQ);
+			IRQ_SetMode((IRQn_ID_t)irqn, IRQ_MODE_TRIG_EDGE_RISING | IRQ_MODE_CPU_0 /*ALL*/ | IRQ_MODE_TYPE_IRQ);
 		IRQ_Enable(irqn);
 	}
 };
