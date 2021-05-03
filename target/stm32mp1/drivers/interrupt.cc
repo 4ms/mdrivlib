d39915df9f99a9589f8dc2786a227848747cceec
Dan Green <danngreen1@gmail.com>
2021-05-03 14:14:59 -0700

    Cortex-A7 IRQ_Handler: push only S0-S31, and don't try to handle ISRs# > 1020

--- target/stm32mp1/drivers/interrupt.cc
+++ target/stm32mp1/drivers/interrupt.cc
@@ -24,8 +24,8 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		"push {r3}  					\n" // Store adjustment to stack
 											//////////FPU
 		"vmrs r1, FPSCR 				\n" // Copy FPU status reg to r1
-		"vpush {d0-d15} 				\n" // Push all double and single registers
-		"vpush {d16-d31} 				\n"
+		// "vpush {d0-d15} 				\n" // Push all double and single registers
+		// "vpush {d16-d31} 				\n"
 		"vpush {s0-s15} 				\n"
 		"vpush {s16-s31} 				\n"
 		"push {r1} 						\n" // Push the FPU status reg
@@ -34,11 +34,14 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		// The following block used to be 'bl IRQ_GetActiveIRQ', which contained an Errata fix
 		"mov r3, #GICCPU_BASE_low		\n"
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
-		"ldr r2, [r3, #24] 				\n" // Get Highest Pending Interrupt number
-		"ldr r2, [r3, #12]				\n" // Acknowledge it with a read to the Interrupt Acknowledge Register
+		"ldr r2, [r3, #24] 				\n" // HPPIR: Get Highest Pending Interrupt number
+		"ldr r2, [r3, #12]				\n" // IAR: Acknowledge it with a read to the Interrupt Acknowledge Register
 		"ubfx r0, r2, #0, #10 			\n" // Mask IRQ number to 0..1023
 		// "uxth r0, r2 					\n" // Grab just the bottom 16bits
 		"dsb sy							\n" // Data barrier (Todo: research why)
+
+		"cmp r0, #0x03fc 				\n" // Check if it's a valid IRQ number
+		"bge InvalidIRQNum 				\n" // Skip this if it's invalid
 
 		"push {r0} 						\n" // Push IRQ number (r0) so ISRHandler doesn't overwrite
 		"cpsie i 						\n" // Enable interrupts
@@ -46,22 +49,20 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		"pop {r0} 						\n" // Restore the IRQ number
 
 		// The following block was 'bl IRQ_EndOfInterrupt', which contained an Errata fix
-		"ubfx r2, r0, #0, #10 			\n" // Mask IRQ number to 0..1023
-		"cmp r2, #0x03fc 				\n" // Check if it's a valid IRQ number
-		"bge InvalidIRQNum 				\n" // Skip this if it's invalid
 		"mov r3, #GICCPU_BASE_low		\n" //
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
-		"str r2, [r3, #16] 				\n" // Write IRQ num to End of Interrupt Register to tell GIC we're done
-		"InvalidIRQNum: 				\n"
+		"str r0, [r3, #16] 				\n" // EOIR: Write IRQ num to End of Interrupt Register to tell GIC we're done
 
 		"cpsid i 						\n" // Disable interrupts while we exit
+
+		"InvalidIRQNum: 				\n"
 
 		//////////FPU
 		"pop {r1} 						\n" // Pop the FPU status reg
 		"vpop {s16-s31} 				\n"
 		"vpop {s0-s15} 					\n"
-		"vpop {d16-d31} 				\n"
-		"vpop {d0-d15} 					\n" // Push all double and single registers
+		// "vpop {d16-d31} 				\n"
+		// "vpop {d0-d15} 					\n" // Push all double and single registers
 		"vmsr FPSCR, r1 				\n" // Restore FPU status reg from popped r1
 											////////////
 
