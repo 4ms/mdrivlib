0fc4250f11733765ef3bafa17e00d3ec574108f7
Dan Green <danngreen1@gmail.com>
2021-05-07 16:17:50 -0700

    Removed comments

--- target/stm32mp1_cm4/boot/system_startup_m4.hh
+++ target/stm32mp1_cm4/boot/system_startup_m4.hh
@@ -13,15 +13,6 @@ namespace core_a7
 {
 struct SystemClocks {
 	SystemClocks() {
-
-		// Enable notification in order to wakeup
-		// HWSemaphore<15>::enable_channel_ISR();
-		// System::enable_irq(HSEM_IT2_IRQn);
-		// InterruptManager::registerISR(HSEM_IT2_IRQn, 0, 0, []() {
-		// 	HWSemaphore<15>::clear_ISR();
-		// 	System::disable_irq(HSEM_IT2_IRQn);
-		// });
-
 		// Domain D2 goes to STOP mode (Cortex-M4 in deep-sleep) waiting for Cortex-M7 to
 		// perform system initialization
 		// HAL_PWREx_ClearPendingEvent();
@@ -33,15 +24,6 @@ struct SystemClocks {
 		// __HAL_ART_ENABLE();
 
 		NVIC_SetPriorityGrouping(0b101); // 2/2
-
-		// uint32_t common_system_clock;
-		// common_system_clock = HAL_RCC_GetSysClockFreq() >>
-		// 					  ((D1CorePrescTable[(RCC->D1CFGR & RCC_D1CFGR_D1CPRE) >> RCC_D1CFGR_D1CPRE_Pos]) & 0x1FU);
-
-		// SystemD2Clock = (common_system_clock >>
-		// 				 ((D1CorePrescTable[(RCC->D1CFGR & RCC_D1CFGR_HPRE) >> RCC_D1CFGR_HPRE_Pos]) & 0x1FU));
-		// SystemCoreClock = SystemD2Clock;
-		//
 
 		SystemCoreClock = HAL_RCC_GetSystemCoreClockFreq();
 
