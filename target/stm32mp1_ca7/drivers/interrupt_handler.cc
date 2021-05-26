55a76cab294a9e22ddc691754a97c352172ce4b3
Dan Green <danngreen1@gmail.com>
2021-05-25 17:13:04 -0700

    IRQ_Handler is `naked`, other handlers are `used` so linker doesn't remove them
    Pushing double float registers, and disabling IRQ mode right after returning from handler

--- target/stm32mp1_ca7/drivers/interrupt_handler.cc
+++ target/stm32mp1_ca7/drivers/interrupt_handler.cc
@@ -1,16 +1,17 @@
 #include "drivers/interrupt.hh"
 
-extern "C" /*__attribute__((interrupt))*/ void ISRHandler(unsigned irqnum) {
+extern "C" {
+void __attribute__((used)) ISRHandler(unsigned irqnum) {
 	InterruptManager::callISR(irqnum);
 }
 
-extern "C" {
-void __attribute__((interrupt)) SVC_Handler() {
+void __attribute__((used)) SVC_Handler() {
 	__BKPT();
 }
 
-void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Handler() {
+void __attribute__((naked)) __attribute__((section(".irqhandler"))) IRQ_Handler() {
 	asm volatile(
+		".equ MODE_SVC, 0x13  			\n"
 		".equ MODE_SYS, 0x1F  			\n"
 		".equ GICCPU_BASE_low, 0x2000 	\n"
 		".equ GICCPU_BASE_high, 0xA002 	\n"
@@ -18,27 +19,23 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 											// that was about to be executed
 		"srsdb sp!, MODE_SYS 			\n" // Save LR_irq and SPSR_irq onto the System mode stack, (decrement SP_sys)
 		"cps MODE_SYS 		 			\n" // Switch to system mode
-		"push {r0-r3, r12, lr} 			\n" // Store remaining AAPCS registers on the System mode stack
+		"push {r0-r3, r12, lr} 			\n" // Store remaining AAPCS registers on the System mode stack:
+											// these are expected to be clobbered by the bl ISRHandler call
 		"and r3, sp, #4  	 			\n" // Ensure stack is 8-byte aligned.
 		"sub sp, sp, r3  				\n" //
 		"push {r3}  					\n" // Store adjustment to stack
-											//////////FPU
+
+		//////////FPU
 		"vmrs r1, FPSCR 				\n" // Copy FPU status reg to r1
-		// "vpush {d0-d15} 				\n" // Push all double and single registers
-		// "vpush {d16-d31} 				\n"
-		"vpush {s0-s15} 				\n"
-		"vpush {s16-s31} 				\n"
+		"vpush {d0-d15} 				\n" // Push all double and single registers
+		"vpush {d16-d31} 				\n"
 		"push {r1} 						\n" // Push the FPU status reg
 											/////////
 
-		// The following block used to be 'bl IRQ_GetActiveIRQ', which contained an Errata fix
 		"mov r3, #GICCPU_BASE_low		\n"
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
-		"ldr r2, [r3, #24] 				\n" // HPPIR: Get Highest Pending Interrupt number
-		"ldr r2, [r3, #12]				\n" // IAR: Acknowledge it with a read to the Interrupt Acknowledge Register
-		"ubfx r0, r2, #0, #10 			\n" // Mask IRQ number to 0..1023
-		// "uxth r0, r2 					\n" // Grab just the bottom 16bits
-		"dsb sy							\n" // Data barrier (Todo: research why)
+		"ldr r0, [r3, #12]				\n" // IAR: Acknowledge it with a read to the Interrupt Acknowledge Register
+		//"dsb sy							\n" // Data barrier (Todo: research why)
 
 		"cmp r0, #0x03fc 				\n" // Check if it's a valid IRQ number
 		"bge InvalidIRQNum 				\n" // Skip this if it's invalid
@@ -46,23 +43,19 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 		"push {r0} 						\n" // Push IRQ number (r0) so ISRHandler doesn't overwrite
 		"cpsie i 						\n" // Enable interrupts
 		"bl ISRHandler 					\n" // Handle the ISR
+		"cpsid i 						\n" // Disable interrupts while we exit
 		"pop {r0} 						\n" // Restore the IRQ number
 
-		// The following block was 'bl IRQ_EndOfInterrupt', which contained an Errata fix
 		"mov r3, #GICCPU_BASE_low		\n" //
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
 		"str r0, [r3, #16] 				\n" // EOIR: Write IRQ num to End of Interrupt Register to tell GIC we're done
-
-		"cpsid i 						\n" // Disable interrupts while we exit
 
 		"InvalidIRQNum: 				\n"
 
 		//////////FPU
 		"pop {r1} 						\n" // Pop the FPU status reg
-		"vpop {s16-s31} 				\n"
-		"vpop {s0-s15} 					\n"
-		// "vpop {d16-d31} 				\n"
-		// "vpop {d0-d15} 					\n" // Push all double and single registers
+		"vpop {d16-d31} 				\n"
+		"vpop {d0-d15} 					\n" // Push all double and single registers
 		"vmsr FPSCR, r1 				\n" // Restore FPU status reg from popped r1
 											////////////
 
@@ -74,7 +67,7 @@ void __attribute__((interrupt)) __attribute__((section(".irqhandler"))) IRQ_Hand
 	);
 }
 
-void __attribute__((interrupt)) FIQ_Handler() {
+void __attribute__((used)) FIQ_Handler() {
 	__BKPT();
 }
 }
