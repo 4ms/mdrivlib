263489240548c4ec7c5febe0979df4e12dcd22f6
Dan Green <danngreen1@gmail.com>
2021-06-15 13:01:52 -0700

    added more data registers to SMP control

--- target/stm32mp1_ca7/drivers/smp.hh
+++ target/stm32mp1_ca7/drivers/smp.hh
@@ -9,13 +9,31 @@ struct SMPControl {
 			SecondaryCore::send_sgi(SGI1_IRQn);
 		if (channel == 2)
 			SecondaryCore::send_sgi(SGI2_IRQn);
+		if (channel == 3)
+			SecondaryCore::send_sgi(SGI3_IRQn);
 	}
 
-	static uint32_t read() {
-		return TAMP->BKP31R;
-	}
+	// static uint32_t read() {
+	// 	return TAMP->BKP31R;
+	// }
 
+	// static void write(uint32_t value) {
+	// 	TAMP->BKP31R = value;
+	// }
+
+	template<uint32_t reg_num = 31>
 	static void write(uint32_t value) {
-		TAMP->BKP31R = value;
+		if constexpr (reg_num == 31)
+			TAMP->BKP31R = value;
+		else if (reg_num == 30)
+			TAMP->BKP30R = value;
+	}
+
+	template<uint32_t reg_num = 31>
+	static uint32_t read() {
+		if constexpr (reg_num == 31)
+			return TAMP->BKP31R;
+		else if (reg_num == 30)
+			return TAMP->BKP30R;
 	}
 };
