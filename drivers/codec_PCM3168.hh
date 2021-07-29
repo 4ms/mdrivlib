dbec573e68f3b4a6122c79ea1b35075b178af306
Dan Green <danngreen1@gmail.com>
2021-07-28 21:36:50 -0700

    More verbose description of math behind codec rx/tx buffer sizes

--- drivers/codec_PCM3168.hh
+++ drivers/codec_PCM3168.hh
@@ -61,13 +61,17 @@ public:
 
 	template<typename FrameT, size_t BUFSIZE>
 	void set_tx_buffers(std::array<FrameT, BUFSIZE> &tx_buf) {
-		constexpr uint32_t block_size = sizeof(tx_buf) / 2;
+		constexpr uint32_t NumHalfTransfers = 2;
+		constexpr uint32_t BytesPerSample = 4;
+		constexpr uint32_t block_size = sizeof(tx_buf) * NumHalfTransfers / BytesPerSample;
 		sai_.set_tx_buffers(reinterpret_cast<uint8_t *>(tx_buf.data()), block_size);
 	}
 
 	template<typename FrameT, size_t BUFSIZE>
 	void set_rx_buffers(std::array<FrameT, BUFSIZE> &rx_buf) {
-		constexpr uint32_t block_size = sizeof(rx_buf) / 2;
+		constexpr uint32_t NumHalfTransfers = 2;
+		constexpr uint32_t BytesPerSample = 4;
+		constexpr uint32_t block_size = sizeof(rx_buf) * NumHalfTransfers / BytesPerSample;
 		sai_.set_rx_buffers(reinterpret_cast<uint8_t *>(rx_buf.data()), block_size);
 	}
 
