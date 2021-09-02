7b1893821f17b3e5a0555ccc3bcab1528f4da013
Dan Green <danngreen1@gmail.com>
2021-09-02 10:57:00 -0700

    added dual-mux ADC I2C support

--- drivers/adc_i2c_config_struct.hh
+++ drivers/adc_i2c_config_struct.hh
@@ -14,4 +14,14 @@ struct MuxedADC_Config {
 	PinNoInit SEL2;
 	ADC_I2C_Config adc_conf;
 };
+
+struct DualMuxedADC_Config {
+	PinNoInit SEL0;
+	PinNoInit SEL1;
+	PinNoInit SEL2;
+	PinNoInit bSEL0;
+	PinNoInit bSEL1;
+	PinNoInit bSEL2;
+	ADC_I2C_Config adc_conf;
+};
 } // namespace mdrivlib
