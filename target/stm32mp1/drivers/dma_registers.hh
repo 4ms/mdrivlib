0c6a1b374c170c5bea7698ff9126236ee2a1cd35
Dan Green <danngreen1@gmail.com>
2021-10-05 10:51:04 -0700

    Added DMA registers for MP1

---
+++ target/stm32mp1/drivers/dma_registers.hh
@@ -0,0 +1,135 @@
+#pragma once
+#include "register_access.hh"
+#include "stm32xx.h"
+
+namespace mdrivlib
+{
+template<size_t PeriphNum, size_t StreamNum>
+struct DMA_ {
+	static constexpr uint32_t GetStreamBase() {
+		if constexpr (PeriphNum == 1) {
+			return StreamNum == 0 ? DMA1_Stream0_BASE
+				 : StreamNum == 1 ? DMA1_Stream1_BASE
+				 : StreamNum == 2 ? DMA1_Stream2_BASE
+				 : StreamNum == 3 ? DMA1_Stream3_BASE
+				 : StreamNum == 4 ? DMA1_Stream4_BASE
+				 : StreamNum == 5 ? DMA1_Stream5_BASE
+				 : StreamNum == 6 ? DMA1_Stream6_BASE
+								  : DMA1_Stream7_BASE;
+		} else {
+			return StreamNum == 0 ? DMA2_Stream0_BASE
+				 : StreamNum == 1 ? DMA2_Stream1_BASE
+				 : StreamNum == 2 ? DMA2_Stream2_BASE
+				 : StreamNum == 3 ? DMA2_Stream3_BASE
+				 : StreamNum == 4 ? DMA2_Stream4_BASE
+				 : StreamNum == 5 ? DMA2_Stream5_BASE
+				 : StreamNum == 6 ? DMA2_Stream6_BASE
+								  : DMA2_Stream7_BASE;
+		}
+	}
+
+	static constexpr uint32_t GetIFCRBase() {
+		if constexpr (StreamNum < 4) {
+			return BASE + offsetof(DMA_TypeDef, LIFCR);
+		} else {
+			return BASE + offsetof(DMA_TypeDef, HIFCR);
+		}
+	}
+
+	static constexpr uint32_t GetISRBase() {
+		if constexpr (StreamNum < 4) {
+			return BASE + offsetof(DMA_TypeDef, LISR);
+		} else {
+			return BASE + offsetof(DMA_TypeDef, HISR);
+		}
+	}
+
+	// returns the bit number of the lowest register for this stream
+	// in the base DMA's LIFCR/HIFCR or LISR/HISR
+	static constexpr uint32_t GetBaseStreamShift() {
+		return StreamNum == 0 ? 0
+			 : StreamNum == 1 ? 6
+			 : StreamNum == 2 ? 16
+			 : StreamNum == 3 ? 22
+			 : StreamNum == 4 ? 0
+			 : StreamNum == 5 ? 6
+			 : StreamNum == 6 ? 16
+							  : 22;
+	}
+
+	using TransferComplClearFlag =
+		RegisterSection<WriteOnly, GetIFCRBase(), GetBaseStreamShift() + DMA_LIFCR_CTCIF0_Pos, 1>;
+	using HalfTransferComplClearFlag =
+		RegisterSection<WriteOnly, GetIFCRBase(), GetBaseStreamShift() + DMA_LIFCR_CHTIF0_Pos, 1>;
+	using TransferErrorClearFlag =
+		RegisterSection<WriteOnly, GetIFCRBase(), GetBaseStreamShift() + DMA_LIFCR_CTEIF0_Pos, 1>;
+	using DirectModeErrorClearFlag =
+		RegisterSection<WriteOnly, GetIFCRBase(), GetBaseStreamShift() + DMA_LIFCR_CDMEIF0_Pos, 1>;
+	using FIFOErrorClearFlag =
+		RegisterSection<WriteOnly, GetIFCRBase(), GetBaseStreamShift() + DMA_LIFCR_CFEIF0_Pos, 1>;
+
+	static constexpr uint32_t BASE = PeriphNum == 1 ? DMA1_BASE : DMA2_BASE;
+	static constexpr uint32_t STREAM_BASE = GetStreamBase();
+	// typedef struct
+	// {
+	//   __IO uint32_t CR;     /*!< DMA stream x configuration register      */
+	//   __IO uint32_t NDTR;   /*!< DMA stream x number of data register     */
+	//   __IO uint32_t PAR;    /*!< DMA stream x peripheral address register */
+	//   __IO uint32_t M0AR;   /*!< DMA stream x memory 0 address register   */
+	//   __IO uint32_t M1AR;   /*!< DMA stream x memory 1 address register   */
+	//   __IO uint32_t FCR;    /*!< DMA stream x FIFO control register       */
+	// } DMA_Stream_TypeDef;
+
+	static constexpr uint32_t CR_Base = STREAM_BASE + offsetof(DMA_Stream_TypeDef, CR);
+	using Enable = RegisterSection<ReadWrite, CR_Base, DMA_SxCR_EN_Pos, 1>;
+
+	// typedef struct
+	// {
+	//   __IO uint32_t LISR;   /*!< DMA low interrupt status register,      Address offset: 0x00 */
+	//   __IO uint32_t HISR;   /*!< DMA high interrupt status register,     Address offset: 0x04 */
+	//   __IO uint32_t LIFCR;  /*!< DMA low interrupt flag clear register,  Address offset: 0x08 */
+	//   __IO uint32_t HIFCR;  /*!< DMA high interrupt flag clear register, Address offset: 0x0C */
+	//   __IO uint32_t RESERVED[247];  /*!< Reserved,               Address offset: 0x10 - 0x3E8 */
+	//   __IO uint32_t HWCFGR2; /*!< DMA HW Configuration register 2,      Address offset: 0x3EC */
+	//   __IO uint32_t HWCFGR1; /*!< DMA HW Configuration register 1,      Address offset: 0x3F0 */
+	//   __IO uint32_t VERR;    /*!< DMA Version register,                 Address offset: 0x3F4 */
+	//   __IO uint32_t IPDR;    /*!< DMA Identification register,          Address offset: 0x3F8 */
+	//   __IO uint32_t SIDR;    /*!< DMA Size Identification register,     Address offset: 0x3FC */
+	// } DMA_TypeDef;
+
+	// typedef struct
+	// {
+	//   __IO uint32_t  CCR;        /*!< DMA Multiplexer Channel x Control Register   */
+	// }DMAMUX_Channel_TypeDef;
+
+	// typedef struct
+	// {
+	//   __IO uint32_t  CSR;      /*!< DMA Channel Status Register     */
+	//   __IO uint32_t  CFR;      /*!< DMA Channel Clear Flag Register */
+	// }DMAMUX_ChannelStatus_TypeDef;
+
+	// typedef struct
+	// {
+	//   __IO uint32_t  RGCR;        /*!< DMA Request Generator x Control Register   */
+	// }DMAMUX_RequestGen_TypeDef;
+
+	// typedef struct
+	// {
+	//   __IO uint32_t  RGSR;            /*!< DMAMUX Request Generator Status Register,           Address offset: 0x140
+	//   */
+	//   __IO uint32_t  RGCFR;           /*!< DMAMUX Request Generator Clear Flag Register,       Address offset: 0x144
+	//   */ uint32_t       RESERVED0[169];  /*!< Reserved, 0x144 -> 0x144 */
+	//   __IO uint32_t  HWCFGR2;         /*!< DMAMUX Configuration register 2,                    Address offset: 0x3EC
+	//   */
+	//   __IO uint32_t  HWCFGR1;         /*!< DMAMUX Configuration register 1,                    Address offset: 0x3F0
+	//   */
+	//   __IO uint32_t  VERR;            /*!< DMAMUX Verion Register,                             Address offset: 0x3F4
+	//   */
+	//   __IO uint32_t  IPDR;            /*!< DMAMUX Identification register,                     Address offset: 0x3F8
+	//   */
+	//   __IO uint32_t  SIDR;            /*!< DMAMUX Size Identification register,                Address offset: 0x3FC
+	//   */
+
+	// }DMAMUX_RequestGenStatus_TypeDef;
+};
+} // namespace mdrivlib
