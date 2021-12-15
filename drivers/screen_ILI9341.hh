7fb66057be5e18ed63ea60fa2eb31e3fd04f09a0
Dan Green <danngreen1@gmail.com>
2021-12-15 10:18:14 -0800

    Making LTDC Screen init more generic

---
+++ drivers/screen_ILI9341.hh
@@ -0,0 +1,105 @@
+#pragma once
+#include "drivers/screen_ltdc_setup.hh"
+#include "util/math.hh"
+#include <array>
+#include <cstdint>
+#include <span>
+
+namespace mdrivlib::ILI9341
+{
+
+enum Command : uint8_t {
+	Reset = 0x01,
+	SleepOut = 0x11,
+	DisplayOff = 0x28,
+	DisplayOn = 0x29,
+	ColSet = 0x2A,
+	RowSet = 0x2B,
+	ColorMode = 0x3A,
+	PowerControlA = 0xCB,
+	PowerControlB = 0xCF,
+	DriverTimingA = 0xE8,
+	DriverTimingB = 0xEA,
+	PowerOnSequence = 0xED,
+	PumpRatioControl = 0xF7,
+	PowerControl1 = 0xC0,
+	PowerControl2 = 0xC1,
+	VCOMControl1 = 0xC5,
+	VCOMControl2 = 0xC7,
+
+	MemoryAccessControl = 0x36,
+	DisplayNonInvert = 0x20,
+	DisplayInvert = 0x21,
+	PixelFormat = 0x3A,
+
+	FrameRateControl = 0xB1,
+	DisplayFunctionControl = 0xB6,
+
+};
+
+enum InvertState { NotInverted, Inverted };
+
+// TODO:
+// reset high 1ms, low 10ms, high 120ms
+
+struct ILI9341InitSPI {
+	//TODO
+};
+
+//FIXME: Doesn't work!
+// template<uint16_t WIDTH, uint16_t HEIGHT, enum InvertState ISINVERTED>
+struct ILI9341InitLTDC {
+	static constexpr LTDCInitCommand cmds[] = {
+		{.cmd = 0xCF, .num_args = 3, .delay_ms = 0, .args = {0x00, 0xc3, 0x30}},
+		{.cmd = 0xED, .num_args = 4, .delay_ms = 0, .args = {0x64, 0x03, 0x12, 0x81}},
+		{.cmd = 0xE8, .num_args = 3, .delay_ms = 0, .args = {0x85, 0x10, 0x79}},
+		{.cmd = 0xCB, .num_args = 5, .delay_ms = 0, .args = {0x39, 0x2C, 0x00, 0x34, 0x02}},
+		{.cmd = 0xF7, .num_args = 1, .delay_ms = 0, .args = {0x20}},
+		{.cmd = 0xEA, .num_args = 2, .delay_ms = 0, .args = {0x00, 0x00}},
+		//Power control //VRH[5:0]
+		{.cmd = 0xC0, .num_args = 1, .delay_ms = 0, .args = {0x22}},
+		//Power control //SAP[2:0];BT[3:0]
+		{.cmd = 0xC1, .num_args = 1, .delay_ms = 0, .args = {0x11}},
+		//VCM control // 3a	46
+		{.cmd = 0xC5, .num_args = 2, .delay_ms = 0, .args = {0x3a, 0x1c}},
+		//VCM control2 // b0  0xc5	  8b
+		{.cmd = 0xC7, .num_args = 1, .delay_ms = 0, .args = {0xa9}},
+		// Memory Access Control
+		{.cmd = 0x36, .num_args = 1, .delay_ms = 0, .args = {0x08}},
+		{.cmd = 0x3A, .num_args = 1, .delay_ms = 0, .args = {0x55}},
+		//70hz
+		{.cmd = 0xB1, .num_args = 2, .delay_ms = 0, .args = {0x00, 0x1B}},
+		// Display Function Control
+		{.cmd = 0xB6, .num_args = 2, .delay_ms = 0, .args = {0x0A, 0xA2}},
+		{.cmd = 0xF6, .num_args = 2, .delay_ms = 0, .args = {0x01, 0x1d}},
+		// 3Gamma Function Disable
+		{.cmd = 0xF2, .num_args = 1, .delay_ms = 0, .args = {0x00}},
+		//Gamma curve selected
+		// {.cmd = 0x26, .num_args = 1, .delay_ms = 0, .args = {0x01}},
+
+		{.cmd = PixelFormat, .num_args = 1, .delay_ms = 10, .args = {0x55}},
+
+		{.cmd = DisplayInvert, .num_args = 0, .delay_ms = 0},
+		{.cmd = SleepOut, .num_args = 0, .delay_ms = 120},
+		{.cmd = DisplayOn, .num_args = 0, .delay_ms = 50},
+
+		{.cmd = RowSet, .num_args = 4, .delay_ms = 0, .args = {0, (320 >> 8), 0, 320 & 0x00FF}},
+		{.cmd = ColSet, .num_args = 4, .delay_ms = 0, .args = {0, 0, 0, 240}},
+
+		//{.cmd = Reset, .num_args = 0, .delay_ms = 5000},
+		//{.cmd = DisplayOff, .num_args = 0},
+		//{.cmd = PowerControlA, .num_args = 5, .delay_ms = 0, .args = {0x39, 0x2C, 0, 0x34, 2}},
+		//{.cmd = PowerControlB, .num_args = 3, .delay_ms = 0, .args = {0, 0xC1, 0x30}},
+		/////....
+		//{.cmd = MemoryAccessControl, .num_args = 1, .delay_ms = 0, .args = {0x08}},
+		//{.cmd = DisplayNonInvert, .num_args = 0, .delay_ms = 0},
+		// {.cmd = PixelFormat, .num_args = 1, .delay_ms = 10, .args = {0x55}},
+
+		//{.cmd = SleepOut, .num_args = 0, .delay_ms = 120},
+		//{.cmd = DisplayOn, .num_args = 0},
+	};
+
+	static constexpr uint32_t num_commands = sizeof(cmds) / sizeof(LTDCInitCommand);
+};
+
+} // namespace mdrivlib::ILI9341
--- drivers/screen_ltdc_setup.hh
+++ drivers/screen_ltdc_setup.hh
@@ -3,34 +3,40 @@
 #include "drivers/ltdc_screen_config_struct.hh"
 #include "drivers/parallel_writer.hh"
 #include "drivers/pin.hh"
-#include "drivers/spi_screen_ST77XX.hh"
 #include "util/base_concepts.hh"
 #include <span>
 
 namespace mdrivlib
 {
+
+struct LTDCInitCommand {
+	uint8_t cmd;
+	uint8_t num_args = 0;
+	uint16_t delay_ms = 0;
+	uint8_t args[8];
+	// std::span<const uint8_t> args; // FixMe: std::span<uint8_t> or std::span<const uint8_t> gives error: No matching
+	// constructor
+};
+
 template<Derived<ParallelWriterConf> ConfT>
-class ST77XXParallelSetup {
+class LTDCParallelSetup {
 
 	mdrivlib::ParallelWriter<ConfT> writer;
 
 public:
-	void setup_driver_chip(std::span<const ST77XX::InitCommand> cmds) {
+	void setup_driver_chip(std::span<const LTDCInitCommand> cmds) {
 		writer.init_pins();
 		_reset();
 		_init_display_driver(cmds);
 	}
 
 private:
-	void _init_display_driver(std::span<const ST77XX::InitCommand> cmds) {
+	void _init_display_driver(std::span<const LTDCInitCommand> cmds) {
 		writer.start_sequence();
 		for (auto &c : cmds) {
 			writer.send_cmd(c.cmd);
-			int numArgs = c.num_args;
-			uint32_t args = c.args;
-			while (numArgs--) {
-				writer.send_arg(args & 0x000000FF);
-				args >>= 8;
+			for (int i = 0; i < c.num_args; i++) {
+				writer.send_arg(c.args[i]);
 			}
 			if (c.delay_ms)
 				HAL_Delay(c.delay_ms);
@@ -41,14 +47,11 @@ private:
 	void _reset() {
 		Pin reset_pin{ConfT::reset, PinMode::Output};
 		reset_pin.high();
-		volatile int i = 10000; //10000 = high for 123us
-		while (i)
-			i = i - 1;
+		HAL_Delay(1);
 		reset_pin.low();
-		i = 1000; //1000 = low for 12.6us
-		while (i)
-			i = i - 1;
+		HAL_Delay(10);
 		reset_pin.high();
+		HAL_Delay(120);
 	}
 };
 } // namespace mdrivlib
