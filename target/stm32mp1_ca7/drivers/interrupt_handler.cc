5b58f43efcb495c339600f8b1cb0f46910564526
Dan Green <danngreen1@gmail.com>
2022-03-22 11:56:59 -0700

    IRQ Handler optimizations
    consolidating stack push/pops
    Invalid IRQ num doesn't need to deal with FPU regs

--- target/stm32mp1_ca7/drivers/interrupt_handler.cc
+++ target/stm32mp1_ca7/drivers/interrupt_handler.cc
@@ -18,19 +18,9 @@ void __attribute__((naked)) __attribute__((section(".irqhandler"))) IRQ_Handler(
 		"sub lr, lr, #4 				\n" // LR = addr of *next* instr to execute; subtract 4 so we return to instr
 											// that was about to be executed
 		"srsdb sp!, MODE_SYS 			\n" // Save LR_irq and SPSR_irq onto the System mode stack, (decrement SP_sys)
-		"cps MODE_SYS 		 			\n" // Switch to system mode
+		"cps MODE_SYS 		 			\n" // Switch to system mode (because our baremetal app runs in system mode)
 		"push {r0-r3, r12, lr} 			\n" // Store remaining AAPCS registers on the System mode stack:
 											// these are expected to be clobbered by the bl ISRHandler call
-		"and r3, sp, #4  	 			\n" // Ensure stack is 8-byte aligned.
-		"sub sp, sp, r3  				\n" //
-		"push {r2, r3}  				\n" // Store adjustment to stack plus a dummy reg to maintain alignment
-
-		//////////FPU
-		"vmrs r1, FPSCR 				\n" // Copy FPU status reg to r1
-		"vpush {d0-d15} 				\n" // Push all double and single registers
-		"vpush {d16-d31} 				\n"
-		"push {r1} 						\n" // Push the FPU status reg
-											/////////
 
 		"mov r3, #GICCPU_BASE_low		\n"
 		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
@@ -40,27 +30,28 @@ void __attribute__((naked)) __attribute__((section(".irqhandler"))) IRQ_Handler(
 		"cmp r0, #0x03fc 				\n" // Check if it's a valid IRQ number
 		"bge InvalidIRQNum 				\n" // Skip this if it's invalid
 
-		"push {r0} 						\n" // Push IRQ number (r0) so ISRHandler doesn't overwrite
+		"and r2, sp, #4  	 			\n" // Ensure stack is 8-byte aligned.
+		"sub sp, sp, r2  				\n" // r2 contains the stack adjustment
+
+		"vmrs r1, FPSCR 				\n" // Copy FPU status reg to r1
+		"vpush {d0-d15} 				\n" // Push all double and single registers
+		"vpush {d16-d31} 				\n"
+
+		"push {r0-r3}					\n" // Push IRQ number (r0), FPU Status (r1), stack adj. (r2), and GIC base (r3)
 		"cpsie i 						\n" // Enable interrupts
 		"bl ISRHandler 					\n" // Handle the ISR
 		"cpsid i 						\n" // Disable interrupts while we exit
-		"pop {r0} 						\n" // Restore the IRQ number
+		"pop {r0-r3} 					\n" // Restore the IRQ number, FPU status, stack adj. and GIC base
 
-		"mov r3, #GICCPU_BASE_low		\n" //
-		"movt r3, #GICCPU_BASE_high		\n" // Load address of the GIC CPU Interface
 		"str r0, [r3, #16] 				\n" // EOIR: Write IRQ num to End of Interrupt Register to tell GIC we're done
 
-		"InvalidIRQNum: 				\n"
-
-		//////////FPU
-		"pop {r1} 						\n" // Pop the FPU status reg
 		"vpop {d16-d31} 				\n"
 		"vpop {d0-d15} 					\n" // Push all double and single registers
 		"vmsr FPSCR, r1 				\n" // Restore FPU status reg from popped r1
-											////////////
 
-		"pop {r2, r3} 					\n" // Pop the stack adjustment and dummy register
-		"add sp, sp, r3  				\n" // Restore previous stack pointer
+		"add sp, sp, r2  				\n" // Restore previous stack pointer
+
+		"InvalidIRQNum: 				\n"
 		"pop {r0-r3, r12, lr} 			\n" //
 		"rfeia sp! 						\n" // Return to address on stack, and pop SPSR (which restores the en/disable
 											// state of IRQs)
