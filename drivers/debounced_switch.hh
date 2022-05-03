4418d57552b98b55cd1ae925cf80f58bf856e51a
Dan Green <danngreen1@gmail.com>
2022-05-02 17:09:20 -0700

    DebouncedPin uses FPin polarity template param

--- drivers/debounced_switch.hh
+++ drivers/debounced_switch.hh
@@ -8,15 +8,12 @@ namespace mdrivlib
 template<GPIO Gpio, uint16_t PinNum, PinPolarity Polarity>
 struct DebouncedPin : public Debouncer<0x00000001, 0xFFFFFFFE, 0x00000FFF> {
 	DebouncedPin() {
-		FPin<Gpio, PinNum, PinMode::Input> init_pin{Polarity == PinPolarity::Inverted ? PinPull::Up : PinPull::None};
+		FPin<Gpio, PinNum, PinMode::Input, Polarity> init_pin{Polarity == PinPolarity::Inverted ? PinPull::Up
+																								: PinPull::None};
 	}
 
 	void update() {
-		unsigned pin_state;
-		if constexpr (Polarity == PinPolarity::Normal)
-			pin_state = FPin<Gpio, PinNum, PinMode::Input>::read() ? 1 : 0;
-		if constexpr (Polarity == PinPolarity::Inverted)
-			pin_state = FPin<Gpio, PinNum, PinMode::Input>::read() ? 0 : 1;
+		unsigned pin_state = FPin<Gpio, PinNum, PinMode::Input, Polarity>::read() ? 1 : 0;
 		register_state(pin_state);
 	}
 };
