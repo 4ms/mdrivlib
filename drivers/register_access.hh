bac62e061d36d249cf255652b8c1e9485cea98c1
Dan Green <danngreen1@gmail.com>
2022-07-25 11:14:41 -0700

    RegisterAccess: unit tests, fixed unclear usage, rm near-duplicate code
    removed ambigious read/write funcs that don't apply the offset
    Replaced RegisterSection with RegisterBits (determining the mask automatically)
    Added first_bit() constexpr func to determine offset given a mask

--- drivers/register_access.hh
+++ drivers/register_access.hh
@@ -1,26 +1,58 @@
-// Based on github.com/kensmith/cortex-from-scratch
-// Extended and modified by Dan Green.
-// Todo: Seek license/permission?
 #pragma once
 #include <cstdint>
 #include <limits>
 
 namespace mdrivlib
 {
-using regsize_t = uint32_t;
+using regsize_t = unsigned long; //32bits on Cortex-M, 64-bits on x86_64 and ARM64
+
+// Mask Helpers
+// from github.com/kensmith/cortex-from-scratch
+template<regsize_t width>
+struct generate_unshifted_mask_t {
+	enum { value = (generate_unshifted_mask_t<width - 1>::value << 1) | 1 };
+};
+template<>
+struct generate_unshifted_mask_t<0> {
+	enum { value = 0 };
+};
+template<regsize_t offset, regsize_t width>
+struct generate_mask_t {
+	enum { value = generate_unshifted_mask_t<width>::value << offset };
+};
+template<regsize_t offset, regsize_t width>
+constexpr regsize_t mask_v = generate_mask_t<offset, width>::value;
+static_assert(mask_v<2, 4> == 0b111100);
+
+// first_bit():
+// Returns the position of the first set bit (LSB = 0)
+// Returns greater than the register size if no bits are set
+constexpr regsize_t first_bit(regsize_t mask) {
+	regsize_t fb = 0;
+	if (mask == 0)
+		return sizeof(regsize_t) * 8;
+	while ((mask & 1) == 0) {
+		mask >>= 1;
+		fb++;
+	}
+	return fb;
+}
+static_assert(first_bit(0b00000001) == 0);
+static_assert(first_bit(0b00000010) == 1);
+static_assert(first_bit(0x80000000) == 31);
 
 // RegisterBits<>: Bits in register specified by mask
-// todo: Require AccessPolicyT has read() write() set() clear()
 template<typename AccessPolicyT, regsize_t address, regsize_t mask>
 struct RegisterBits {
-	static const regsize_t BaseAddress = address;
-	static const regsize_t Mask = mask;
+	static constexpr regsize_t BaseAddress = address;
+	static constexpr regsize_t Mask = mask;
+	static constexpr regsize_t offset = first_bit(mask);
 
 	static regsize_t read() {
-		return AccessPolicyT::read(reinterpret_cast<volatile regsize_t *>(address), mask);
+		return AccessPolicyT::read(reinterpret_cast<volatile regsize_t *>(address), offset, mask);
 	}
 	static void write(regsize_t val) {
-		AccessPolicyT::write(reinterpret_cast<volatile regsize_t *>(address), mask, val);
+		AccessPolicyT::write(reinterpret_cast<volatile regsize_t *>(address), offset, mask, val);
 	}
 	static void set() {
 		AccessPolicyT::set(reinterpret_cast<volatile regsize_t *>(address), mask);
@@ -30,8 +62,11 @@ struct RegisterBits {
 	}
 };
 
-// target::RCC_Reset::SAI4_::set();
-// target::RCC_Reset_Release::SAI4_::set();
+// RegisterSection<>: Bits in register specified by offset and width
+// Deprecated?
+template<typename AccessPolicyT, regsize_t address, regsize_t offset, regsize_t width>
+using RegisterSection = RegisterBits<AccessPolicyT, address, mask_v<offset, width>>;
+
 // Null register (periph does not exist)
 struct NonexistantRegister {
 	static regsize_t read() {
@@ -45,65 +80,17 @@ struct NonexistantRegister {
 	}
 };
 
-// RegisterSection<>: Bits in register specified by offset and width
-template<regsize_t width>
-struct generate_unshifted_mask_t {
-	enum { value = (generate_unshifted_mask_t<width - 1>::value << 1) | 1 };
-};
-template<>
-struct generate_unshifted_mask_t<0> {
-	enum { value = 0 };
-};
-
-template<regsize_t offset, regsize_t width>
-struct generate_mask_t {
-	enum { value = generate_unshifted_mask_t<width>::value << offset };
-};
-
-template<regsize_t offset, regsize_t width>
-constexpr regsize_t mask_v = generate_mask_t<offset, width>::value;
-
-template<typename AccessPolicyT, regsize_t address, regsize_t offset, regsize_t width>
-struct RegisterSection {
-	static_assert(width > 0, "Register must be at least 1 bit wide");
-	static_assert(width + offset <= std::numeric_limits<regsize_t>::digits,
-				  "Register bits specified by width+offset exceeds size of register");
-
-	static regsize_t read() {
-		return AccessPolicyT::read(reinterpret_cast<volatile regsize_t *>(address), offset, mask_v<offset, width>);
-	}
-
-	static void write(regsize_t val) {
-		return AccessPolicyT::write(
-			reinterpret_cast<volatile regsize_t *>(address), offset, mask_v<offset, width>, val);
-	}
-
-	static void set() {
-		AccessPolicyT::set(reinterpret_cast<volatile regsize_t *>(address), mask_v<offset, width>);
-	}
-
-	static void clear() {
-		AccessPolicyT::clear(reinterpret_cast<volatile regsize_t *>(address), mask_v<offset, width>);
-	}
-};
-
 // Access Policies:
 
 struct ReadOnly {
 	static regsize_t read(volatile regsize_t *address, regsize_t offset, regsize_t mask) {
 		return (*address & mask) >> offset;
 	}
-	static regsize_t read(volatile regsize_t *address, regsize_t mask) {
-		return *address & mask;
-	}
 };
 
 struct WriteOnly {
 	static void write(volatile regsize_t *address, regsize_t offset, regsize_t mask, regsize_t val) {
 		*address = ((val << offset) & mask);
-	}
-	static void write(volatile regsize_t *address, regsize_t mask, regsize_t val) {
-		*address = (val & mask);
 	}
 	static void set(volatile regsize_t *address, regsize_t mask) {
 		*address = mask;
@@ -113,9 +100,6 @@ struct WriteOnly {
 struct ReadWrite : ReadOnly {
 	static void write(volatile regsize_t *address, regsize_t offset, regsize_t mask, regsize_t val) {
 		*address = (*address & ~mask) | ((val << offset) & mask);
-	}
-	static void write(volatile regsize_t *address, regsize_t mask, regsize_t val) {
-		*address = (*address & ~mask) | (val & mask);
 	}
 	static void set(volatile regsize_t *address, regsize_t mask) {
 		*address = *address | mask;
@@ -136,4 +120,9 @@ struct RegisterDualSetClear {
 		WriteOnly::set(reinterpret_cast<volatile regsize_t *>(ClearAddress), ClearMask);
 	}
 };
+
+// RegisterSetClear<>: shortcut for RegisterDualSetClear by specifying types
+template<typename SetT, typename ClearT>
+using RegisterSetClear = RegisterDualSetClear<SetT::BaseAddress, SetT::Mask, ClearT::BaseAddress, ClearT::Mask>;
+
 } // namespace mdrivlib
---
+++ tests/register_access_tests.cc
@@ -0,0 +1,21 @@
+#include "doctest.h"
+#include "register_access.hh"
+#include <memory>
+
+TEST_CASE("register accesss") {
+	using namespace mdrivlib;
+
+	// FIXME: how to get the address of a variable in a constexpr way?
+	// int REG;
+	// using X = RegisterBits<ReadWrite, std::addressof(REG), 0x000FFF00>;
+
+	CHECK(0b1110000 == mask_v<4, 3>);
+	CHECK(0b0001111 == mask_v<0, 4>);
+
+	CHECK(first_bit(0xFFFFFFFF) == 0);
+	CHECK(first_bit(0x7FFFFFFF) == 0);
+	CHECK(first_bit(0b00111100) == 2);
+	CHECK(first_bit(0b0011110) == 1);
+	CHECK(first_bit(0b001111) == 0);
+	CHECK(first_bit(0) == sizeof(unsigned long) * 8);
+}
