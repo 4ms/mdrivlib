02eb52d41f8be7941522a15bc2da3d1bc378051c
Dan Green <danngreen1@gmail.com>
2022-11-06 10:48:23 -0800

    FUSB302 device uses I2CPeriph read_reg/write_reg

--- drivers/fusb302.hh
+++ drivers/fusb302.hh
@@ -3,8 +3,8 @@
 #include "fusb302_registers.hh"
 #include <cstdint>
 
-//Debugging:
-#define DEBUG
+// Debugging:
+// #define DEBUG
 #ifdef DEBUG
 #include "printf.h"
 #define pr_debug printf_
@@ -65,7 +65,8 @@ struct Device {
 		write<Control2>({.Toggle = 1, .PollingMode = Control2::PollDRP, .ToggleIgnoreRa = 1});
 
 		write<Switches0>({.ConnectVConnCC1 = 0, .ConnectVConnCC2 = 0});
-		//Note: setting Mask::VBusOK to 0 when HostCurrentReq is 0 results in it not detecting disconnect as host
+		// Note: setting Mask::VBusOK to 0 when HostCurrentReq is 0 results in it
+		// not detecting disconnect as host
 		write<Mask>({.HostCurrentReq = 0,
 					 .Collision = 1,
 					 .Wake = 1,
@@ -102,7 +103,6 @@ struct Device {
 		pr_debug("Int = 0x%x VBusOK=%d, BCLVL=%d\n", (uint8_t)intr, intr.VBusOK, intr.BCLevel);
 		pr_debug("IntA = 0x%x TogDone=%d\n", intrA, intrA.ToggleDone);
 
-		//TODO: update state based on interrupt register contents
 		switch (state) {
 			case ConnectedState::TogglePolling: {
 				pr_debug("State is currently Polling\n");
@@ -114,9 +114,10 @@ struct Device {
 				else if (status1a.ToggleOutcomeIsCC1 || status1a.ToggleOutcomeIsCC2)
 					state = ConnectedState::AsHost;
 
-				//could also check Status0: Comp == 0 && BCLevel < 3
-				//Comp == 0 means CC pin is read as less than reference, meaning device Rd pull-down was detected
-				//BC<3 means CC pin is read as < 1.23V, meaning a device Rd pull-down was detected
+				// could also check Status0: Comp == 0 && BCLevel < 3
+				// Comp == 0 means CC pin is read as less than reference, meaning device
+				// Rd pull-down was detected BC<3 means CC pin is read as < 1.23V, meaning
+				// a device Rd pull-down was detected
 			} break;
 
 			case ConnectedState::AsDevice: {
@@ -124,7 +125,8 @@ struct Device {
 				Status0 status0{read<Status0>()};
 
 				// Look for unplug event:
-				// VBusOK = 0 means no VBUS, BCLevel == 0 means CC detected as low (no host pull-up detected)
+				// VBusOK = 0 means no VBUS, BCLevel == 0 means CC detected as low (no
+				// host pull-up detected)
 				if (status0.VBusOK == 0 && status0.BCLevel == 0)
 					state = ConnectedState::None;
 			} break;
@@ -133,8 +135,9 @@ struct Device {
 				pr_debug("State is currently Host\n");
 				Status0 status{read<Status0>()};
 				// Look for Unplug event:
-				// Comp==1, BC==3 means CC pin is read as > 1.23V, meaning no device Rd pull-down
-				//FIXME: why isn't Comp set at this point? it gets set a moment later...
+				// Comp==1, BC==3 means CC pin is read as > 1.23V, meaning no device Rd
+				// pull-down
+				// FIXME: why isn't Comp set at this point? it gets set a moment later...
 				if (/*status.Comp == 1 &&*/ status.BCLevel == 3)
 					state = ConnectedState::None;
 			} break;
@@ -145,32 +148,29 @@ struct Device {
 		}
 	}
 
-	// Returns true on sucess
+	// Returns true on success
 	template<typename Reg>
-		requires std::derived_from<Reg, BusReg::WriteAccess> bool
-	write(Reg data) {
-		auto d = static_cast<uint8_t>(data);
-		return i2c.mem_write(dev_addr, Reg::Address, 1, &d, 1) == mdrivlib::I2CPeriph::I2C_NO_ERR;
+	bool write(Reg data) {
+		return i2c.write_reg(dev_addr, data) == mdrivlib::I2CPeriph::I2C_NO_ERR;
 	}
 
-	//TODO: return std::expected<uint8_t> when compiler supports it
 	template<typename Reg>
-		requires std::derived_from<Reg, BusReg::ReadAccess>
 	Reg read() {
 		static bool got_error_once = false;
-		uint8_t data = 0;
-		auto err = i2c.mem_read(dev_addr, Reg::Address, 1, &data, 1);
+		auto reg = i2c.read_reg<Reg>(dev_addr);
 
-		if (err != mdrivlib::I2CPeriph::I2C_NO_ERR && !got_error_once) {
-			pr_debug("Error reading Reg 0x%x: %d\n", Reg::Address, err);
+		if (!reg.has_value() && !got_error_once) {
+			pr_debug("Error reading Reg 0x%x\n", Reg::Address);
 			got_error_once = true;
 		}
-		return Reg::make(data);
+		return reg.value_or(Reg::make(0));
 	}
 
+	//// Debugging stuff:
+#ifdef DEBUG
 	template<typename Reg>
-		requires std::derived_from<Reg, BusReg::ReadAccess>
-	void reg_check(std::string_view regname) {
+	requires std::derived_from<Reg, BusReg::ReadAccess>
+	void reg_check_changed(std::string_view regname) {
 		static uint8_t last_val = 0xFF;
 		auto val = read<Reg>();
 		if ((uint8_t)val != last_val)
@@ -179,10 +179,12 @@ struct Device {
 	}
 
 	template<typename Reg>
-		requires std::derived_from<Reg, BusReg::ReadAccess>
+	requires std::derived_from<Reg, BusReg::ReadAccess>
 	void reg_dump(std::string_view regname) {
 		auto val = read<Reg>();
 		pr_debug("%s: 0x%x\n", regname.data(), val);
+
+		// Dump individual fields of interest:
 		if constexpr (static_cast<uint8_t>(Reg::Address) == FUSB302::Status0::Address) {
 			auto s = Status0::make(val);
 			pr_debug("   BCLevel=%d Wake=%d Comp=%d VBusOK=%d\n", s.BCLevel, s.Wake, s.Comp, s.VBusOK);
@@ -190,31 +192,28 @@ struct Device {
 	}
 
 	void check_all_regs() {
-#ifdef DEBUG
-		reg_check<ID>("ID");
-		reg_check<Switches0>("Switches0");
-		reg_check<Switches1>("Switches1");
-		reg_check<Measure>("Measure");
-		reg_check<Slice>("Slice");
-		reg_check<Control0>("Control0");
-		reg_check<Control1>("Control1");
-		reg_check<Control2>("Control2");
-		reg_check<Control3>("Control3");
-		reg_check<Mask>("Mask1");
-		reg_check<Power>("Power");
-		reg_check<OCP>("OCP");
-		reg_check<MaskA>("MaskA");
-		reg_check<MaskB>("MaskB");
-		reg_check<Control4>("Control4");
-		reg_check<Status0A>("Status0A");
-		reg_check<Status1A>("Status1A");
-		reg_check<Status0>("Status0");
-		reg_check<Status1>("Status1");
-#endif
+		reg_check_changed<ID>("ID");
+		reg_check_changed<Switches0>("Switches0");
+		reg_check_changed<Switches1>("Switches1");
+		reg_check_changed<Measure>("Measure");
+		reg_check_changed<Slice>("Slice");
+		reg_check_changed<Control0>("Control0");
+		reg_check_changed<Control1>("Control1");
+		reg_check_changed<Control2>("Control2");
+		reg_check_changed<Control3>("Control3");
+		reg_check_changed<Mask>("Mask1");
+		reg_check_changed<Power>("Power");
+		reg_check_changed<OCP>("OCP");
+		reg_check_changed<MaskA>("MaskA");
+		reg_check_changed<MaskB>("MaskB");
+		reg_check_changed<Control4>("Control4");
+		reg_check_changed<Status0A>("Status0A");
+		reg_check_changed<Status1A>("Status1A");
+		reg_check_changed<Status0>("Status0");
+		reg_check_changed<Status1>("Status1");
 	}
 
 	void dump_all_regs() {
-#ifdef DEBUG
 		reg_dump<ID>("ID");
 		reg_dump<Switches0>("Switches0");
 		reg_dump<Switches1>("Switches1");
@@ -234,8 +233,13 @@ struct Device {
 		reg_dump<Status1A>("Status1A");
 		reg_dump<Status0>("Status0");
 		reg_dump<Status1>("Status1");
-#endif
 	}
+#else
+	void dump_all_regs() {
+	}
+	void check_all_regs() {
+	}
+#endif
 };
 
 } // namespace FUSB302
