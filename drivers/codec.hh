c7411d2632b803311f65f51f96ef9908ab4a6c88
Dan Green <danngreen1@gmail.com>
2023-01-17 10:58:48 -0800

    codec: documented block size

--- drivers/codec.hh
+++ drivers/codec.hh
@@ -3,7 +3,6 @@
 #include "drivers/sai_config_struct.hh"
 #include "drivers/sai_tdm.hh"
 #include <array>
-#include <cstdint>
 
 namespace mdrivlib
 {
@@ -16,32 +15,17 @@ public:
 		: sai_{sai_def} {
 	}
 
-	template<typename FrameT, size_t NumFramesInBuffer>
-	void set_tx_buffer_start(std::array<FrameT, NumFramesInBuffer> &tx_buf) {
-		// template<typename FrameT>
-		// void set_tx_buffer_start(std::span<FrameT> tx_buf) {
-		//FIXME: This equation is wrong but result is right:
-		//??? size_bytes(buffer) / 2
-		// sizeof(tx_buf) == tx_buf.size() * sizeof(FrameT)
-		// ??block_size = tx_buf.size() * sizeof(FrameT)[==8] / NumHalfTransfers[==2]
-		// ??block_size = (tx_buf.size() / 2) * 8 = Number of Frames * 4
-		// ??the var is not "block_size", it's "number_data_transfers"
-		// ??which equals Number of 32-bit words transferred
-		// which equals NumberofFrames in buffer * Number of channels * bytes per channel / 4
-		// = Number of Frames * 2
-		constexpr uint32_t NumHalfTransfers = 2;
-		constexpr uint32_t BytesPerSample = 4;
-		constexpr uint32_t block_size = sizeof(tx_buf) * NumHalfTransfers / BytesPerSample;
+	template<typename T, size_t NumFramesInBuffer>
+	void set_tx_buffer_start(std::array<T, NumFramesInBuffer> &tx_buf) {
+		constexpr uint32_t BytesPerDMAXfer = 4; //must match DMA MemAlign
+		constexpr uint32_t block_size = sizeof(tx_buf) / BytesPerDMAXfer;
 		sai_.set_tx_buffer_start(reinterpret_cast<uint8_t *>(tx_buf.data()), block_size);
 	}
 
-	template<typename FrameT, size_t BUFSIZE>
-	void set_rx_buffer_start(std::array<FrameT, BUFSIZE> &rx_buf) {
-		// template<typename FrameT>
-		// void set_rx_buffer_start(std::span<FrameT> rx_buf) {
-		constexpr uint32_t NumHalfTransfers = 2;
-		constexpr uint32_t BytesPerSample = 4;
-		constexpr uint32_t block_size = sizeof(rx_buf) * NumHalfTransfers / BytesPerSample;
+	template<typename T, size_t BUFSIZE>
+	void set_rx_buffer_start(std::array<T, BUFSIZE> &rx_buf) {
+		constexpr uint32_t BytesPerDMAXfer = 4; //must match DMA MemAlign
+		constexpr uint32_t block_size = sizeof(rx_buf) / BytesPerDMAXfer;
 		sai_.set_rx_buffer_start(reinterpret_cast<uint8_t *>(rx_buf.data()), block_size);
 	}
 
