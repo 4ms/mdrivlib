8ba7b5554dba916034fe3415333de2f24b819f86
Dan Green <danngreen1@gmail.com>
2023-02-21 15:31:39 -0800

    IPCC interface: including HalfDuplex mode

--- target/stm32mp1/drivers/ipcc.hh
+++ target/stm32mp1/drivers/ipcc.hh
@@ -1,124 +1,108 @@
 #pragma once
-#include "drivers/register_access.hh"
-#include "stm32mp1xx_ll_ipcc.h"
+#include "drivers/interrupt.hh"
+#include "ipcc_regs.hh"
 
 namespace mdrivlib
 {
-struct IPCCRegs {
-	using Core1TXFreeIntEn = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1CR), IPCC_C1CR_TXFIE>;
-	using Core2TXFreeIntEn = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2CR), IPCC_C2CR_TXFIE>;
-	using Core1RXOccIntEn = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1CR), IPCC_C1CR_RXOIE>;
-	using Core2RXOccIntEn = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2CR), IPCC_C2CR_RXOIE>;
+template<size_t N>
+struct IPCC_ {
+	static_assert(N == 1 || N == 2, "IPCC has Core = 1 and Core = 2 only");
 
-	template<size_t CoreNum>
-	struct Core;
-
-	template<>
-	struct Core<1> {
-		using TXFreeIntEn = Core1TXFreeIntEn;
-		using RXOccIntEn = Core1RXOccIntEn;
-
-		// Auto-generated with:
-		// s/\v#define IPCC_([^_]{-})_([^_]{-}) /using \2 = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, \1), IPCC_\1_\2>;\r\0/
-		// RW vs RO access modified manually
-		using CH1C = RegisterBits<WriteOnly, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH1C>;
-		using CH1F = RegisterBits<ReadOnly, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH1F>;
-		using CH1FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH1FM>;
-		using CH1OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH1OM>;
-		using CH1S = RegisterBits<WriteOnly, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH1S>;
-
-		using CH2C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH2C>;
-		using CH2F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH2F>;
-		using CH2FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH2FM>;
-		using CH2OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH2OM>;
-		using CH2S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH2S>;
-		using CH3C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH3C>;
-		using CH3F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH3F>;
-		using CH3FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH3FM>;
-		using CH3OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH3OM>;
-		using CH3S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH3S>;
-		using CH4C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH4C>;
-		using CH4F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH4F>;
-		using CH4FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH4FM>;
-		using CH4OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH4OM>;
-		using CH4S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH4S>;
-		using CH5C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH5C>;
-		using CH5F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH5F>;
-		using CH5FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH5FM>;
-		using CH5OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH5OM>;
-		using CH5S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH5S>;
-		using CH6C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH6C>;
-		using CH6F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1TOC2SR), IPCC_C1TOC2SR_CH6F>;
-		using CH6FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH6FM>;
-		using CH6OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1MR), IPCC_C1MR_CH6OM>;
-		using CH6S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH6S>;
-
-		template<size_t ChanNum>
-		struct Chan {
-			static_assert(ChanNum >= 1 && ChanNum <= 6, "Channel 1 to 6 only allowed");
-			using SetFlag =
-				RegisterBits<WriteOnly, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH1S << (ChanNum - 1)>;
-			using ClrFlag =
-				RegisterBits<WriteOnly, IPCC_BASE + offsetof(IPCC_TypeDef, C1SCR), IPCC_C1SCR_CH1C << (ChanNum - 1)>;
-			using ChangeFlag = RegisterSetClear<SetFlag, ClrFlag>;
-		};
-	};
-
-	template<>
-	struct Core<2> {
-		using TXFreeIntEn = Core2TXFreeIntEn;
-		using RXOccIntEn = Core2RXOccIntEn;
-
-		// Auto-generated with:
-		// s/\v#define IPCC_([^_]{-})_([^_]{-}) /using \2 = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, \1), IPCC_\1_\2>;\r\0/
-		using CH1C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH1C>;
-		using CH1F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH1F>;
-		using CH1FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH1FM>;
-		using CH1OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH1OM>;
-		using CH1S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH1S>;
-		using CH2C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH2C>;
-		using CH2F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH2F>;
-		using CH2FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH2FM>;
-		using CH2OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH2OM>;
-		using CH2S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH2S>;
-		using CH3C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH3C>;
-		using CH3F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH3F>;
-		using CH3FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH3FM>;
-		using CH3OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH3OM>;
-		using CH3S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH3S>;
-		using CH4C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH4C>;
-		using CH4F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH4F>;
-		using CH4FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH4FM>;
-		using CH4OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH4OM>;
-		using CH4S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH4S>;
-		using CH5C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH5C>;
-		using CH5F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH5F>;
-		using CH5FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH5FM>;
-		using CH5OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH5OM>;
-		using CH5S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH5S>;
-		using CH6C = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH6C>;
-		using CH6F = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2TOC1SR), IPCC_C2TOC1SR_CH6F>;
-		using CH6FM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH6FM>;
-		using CH6OM = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2MR), IPCC_C2MR_CH6OM>;
-		using CH6S = RegisterBits<ReadWrite, IPCC_BASE + offsetof(IPCC_TypeDef, C2SCR), IPCC_C2SCR_CH6S>;
-	};
-	//Usage: Core<N>::TXFreeIntEn::set();
-};
-
-template<size_t Core>
-struct IPCC_;
-
-template<>
-struct IPCC_<1> {
-	static void EnableTXFreeIntr() {
-		IPCC->C1CR = IPCC->C1CR | IPCC_C1CR_TXFIE;
+	// Set/Clear the IPCC Flag:
+	template<uint32_t C>
+	static void clear_flag() {
+		IPCCRegs::Core<N>::template Chan<C>::ChangeFlag::clear();
 	}
-	static void DisbleTXFreeIntr() {
-		IPCC->C1CR = IPCC->C1CR | IPCC_C1CR_TXFIE;
+	template<uint32_t C>
+	static void set_flag() {
+		IPCCRegs::Core<N>::template Chan<C>::ChangeFlag::set();
 	}
-};
 
-template<>
-struct IPCC_<2> {};
+	// TXFree:
+	template<uint32_t C>
+	static void enable_chan_txfree_isr() {
+		IPCCRegs::Core<N>::template Chan<C>::FreeISRMasked::clear(); //enable = clear a mask bit (unmask)
+	}
+	template<uint32_t C>
+	static void enable_chan_txfree_isr(uint32_t pri, uint32_t subpri, auto &&callback) {
+		IPCCRegs::Core<N>::TXFreeISREnable::set();
+		enable_chan_txfree_isr<C>();
+		InterruptManager::register_and_start_isr(N == 1 ? IPCC_TX0_IRQn : IPCC_TX1_IRQn, pri, subpri, callback);
+	}
+	template<uint32_t C>
+	static void disable_chan_txfree_isr() {
+		IPCCRegs::Core<N>::template Chan<C>::FreeISRMasked::set();
+	}
+	static void disble_all_txfree_isr() {
+		IPCCRegs::Core<N>::TXFreeISREnable::clear();
+	}
+	template<uint32_t C>
+	static bool is_tx_free() {
+		return IPCCRegs::Core<N>::template Chan<C>::FlagStatus::read() == 0;
+	}
+
+	// RXOccupied:
+	template<uint32_t C>
+	static void enable_chan_rxocc_isr() {
+		IPCCRegs::Core<N>::template Chan<C>::OccISRMasked::clear(); //enable = clear a mask bit (unmask)
+	}
+	template<uint32_t C>
+	static void enable_chan_rxocc_isr(uint32_t pri, uint32_t subpri, auto &&callback) {
+		IPCCRegs::Core<N>::RXOccISREnable::set();
+		enable_chan_rxocc_isr<C>();
+		InterruptManager::register_and_start_isr(N == 1 ? IPCC_RX0_IRQn : IPCC_RX1_IRQn, pri, subpri, callback);
+	}
+	template<uint32_t C>
+	static void disable_chan_rxocc_isr() {
+		IPCCRegs::Core<N>::template Chan<C>::OccISRMasked::set(); //disable = set a mask bit
+	}
+	static void disble_all_rxocc_isr() {
+		IPCCRegs::Core<N>::RXOccISREnable::clear(); //disable = clear an enable bit
+	}
+	template<uint32_t C>
+	static bool is_rx_occupied() {
+		return IPCCRegs::Core<N>::template Chan<C>::FlagStatus::read() != 0;
+	}
+
+	//Half Duplex mode:
+	template<uint32_t C>
+	struct HalfDuplexMode {
+		static auto is_my_turn() {
+			if constexpr (N == 1)
+				return is_tx_free<C>();
+			else
+				return is_rx_occupied<C>();
+		}
+
+		static void enable_chan_isr(uint32_t pri, uint32_t subpri, auto &&callback) {
+			if constexpr (N == 1)
+				enable_chan_txfree_isr<C>(pri, subpri, callback);
+			else
+				enable_chan_rxocc_isr<C>(pri, subpri, callback);
+		}
+
+		static auto notify_other() {
+			if constexpr (N == 1)
+				set_flag<C>();
+			else
+				clear_flag<C>();
+			enable_chan_isr();
+		}
+
+		static auto enable_chan_isr() {
+			if constexpr (N == 1)
+				enable_chan_txfree_isr<C>();
+			else
+				enable_chan_rxocc_isr<C>();
+		}
+
+		static auto disable_chan_isr() {
+			if constexpr (N == 1)
+				disable_chan_txfree_isr<C>();
+			else
+				disable_chan_rxocc_isr<C>();
+		}
+	};
+};
 
 } // namespace mdrivlib
