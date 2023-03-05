f750ed787b4198ed49da568d8ffce9c3997e40eb
Dan Green <danngreen1@gmail.com>
2023-03-05 12:25:17 -0800

    A7 startup: set MCU to run on HSI before configuring clocks
    This prevents HAL returning an error on a warm boot, since MCU is running on PLL3 and we try to configure PLL3

--- target/stm32mp1_ca7/drivers/system_startup.hh
+++ target/stm32mp1_ca7/drivers/system_startup.hh
@@ -11,6 +11,19 @@ struct SystemStartup {
 							const RCC_PeriphCLKInitTypeDef &pclk_def,
 							const uint32_t systick_freq_hz = 1000) {
 
+		// First, change the MCU clock source to HSI, so that we can modify PLL3
+		// This allows us to reboot without a hard power cycle
+		RCC_ClkInitTypeDef rcc_clk_deinit_mcu_conf = {
+			.ClockType = RCC_CLOCKTYPE_HCLK,
+			.MCUInit =
+				{
+					.MCU_Clock = RCC_MCUSSOURCE_HSI,
+					.MCU_Div = RCC_MCU_DIV1,
+				},
+		};
+		if (HAL_RCC_ClockConfig(&rcc_clk_deinit_mcu_conf) != HAL_OK)
+			__BKPT();
+
 		RCC_OscInitTypeDef osc_def_ = osc_def;
 		if (HAL_RCC_OscConfig(&osc_def_) != HAL_OK)
 			__BKPT();
