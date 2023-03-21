5336082dbcbfb11e15b85d9e198d290067215f33
Dan Green <danngreen1@gmail.com>
2023-03-20 21:10:34 -0700

    Added DebouncedButton (counts how long its down)

--- drivers/debounced_switch.hh
+++ drivers/debounced_switch.hh
@@ -27,4 +27,29 @@ struct DebouncedPin : public Debouncer<RisingEdgePattern, FallingEdgePattern, St
 template<PinDef Pindef, PinPolarity Polarity>
 using DebouncedSwitch = DebouncedPin<Pindef, Polarity>;
 
+//
+// DebouncedButton: same as DebouncedSwitch/Pin but counts how long is held down or up
+//
+template<PinDef Pindef,
+		 PinPolarity Polarity,
+		 unsigned RisingEdgePattern = 0x00000001,
+		 unsigned FallingEdgePattern = 0xFFFFFFFE,
+		 unsigned StateMask = 0x00000FFF>
+struct DebouncedButton : DebouncerCounter<RisingEdgePattern, FallingEdgePattern, StateMask> {
+	using PinT = FPin<Pindef.gpio, Pindef.pin, PinMode::Input, Polarity>;
+
+	void update() {
+		unsigned pin_state = PinT::read() ? 1 : 0;
+		this->register_state(pin_state);
+	}
+
+	unsigned how_long_held() {
+		return this->steady_state_ctr;
+	}
+
+	void reset_hold_ctr() {
+		this->steady_state_ctr = 0;
+	}
+};
+
 } // namespace mdrivlib
