5f86c31c0873b67c081561c5b50f850773fd38fe
Dan Green <danngreen1@gmail.com>
2023-11-02 09:16:20 -0700

    Improve cleaning data cache by range

--- target/stm32mp1_ca7/drivers/cache.hh
+++ target/stm32mp1_ca7/drivers/cache.hh
@@ -23,11 +23,14 @@ static constexpr uint32_t CacheLineMask = ~(CacheLineBytes - 1);
 inline void invalidate_dcache_by_range(void *addr, uint32_t size) {
 	// starting address of cache line containing the data
 	auto start_addr = reinterpret_cast<uint32_t>(addr) & CacheLineMask;
-	// ending address of cache line containing the data
+
+	// starting address of next cache line after end of the data
 	auto end_addr = (reinterpret_cast<uint32_t>(addr) + size + CacheLineBytes - 1) & CacheLineMask;
 
-	for (uint32_t addr = start_addr; addr < end_addr; addr += CacheLineBytes)
+	for (uint32_t addr = start_addr; addr < end_addr; addr += CacheLineBytes) {
 		L1C_InvalidateDCacheMVA(reinterpret_cast<void *>(addr));
+	}
+	__DSB();
 }
 
 inline void clean_dcache() {
@@ -39,18 +42,29 @@ inline void clean_dcache_by_addr(ptr addr) {
 	L1C_CleanDCacheMVA(reinterpret_cast<void *>(addr));
 }
 
-inline void clean_dcache_by_range(void *addr, int32_t size) {
-	auto addr_line = reinterpret_cast<uint32_t>(addr) & ~0b11111; // just keep Tag and Set/Index
-	// auto *u32_ptr = reinterpret_cast<uint32_t *>(addr_line);
-	// auto *u32_ptr = reinterpret_cast<uint32_t *>(addr);
-	while (size > 0) {
-		__set_DCCMVAC(addr_line);
-		addr_line += 0b100000;
-		// __set_DCCMVAC((uint32_t)u32_ptr);
-		// u32_ptr += 1;
-		size -= 4;
+inline void clean_dcache_by_range(void *addr, uint32_t size) {
+	// starting address of cache line containing the data
+	auto start_addr = reinterpret_cast<uint32_t>(addr) & CacheLineMask;
+
+	// starting address of next cache line after end of the data
+	auto end_addr = (reinterpret_cast<uint32_t>(addr) + size + CacheLineBytes - 1) & CacheLineMask;
+
+	for (uint32_t addr = start_addr; addr < end_addr; addr += CacheLineBytes) {
+		// L1C_CleanDCacheMVA(reinterpret_cast<void *>(addr));
+		__set_DCCMVAC(addr);
+		__DMB();
 	}
-	__DMB();
+	__DSB();
+
+	// TODO: this was used, but seems to be based on 32-byte lines and seems to invalidate each line 8 times
+	// Makes sure the new code above works
+	// auto addr_line = reinterpret_cast<uint32_t>(addr) & ~0b11111; // just keep Tag and Set/Index
+	// while (size > 0) {
+	// 	__set_DCCMVAC(addr_line);
+	// 	addr_line += 0b100000;
+	// 	size -= 4;
+	// }
+	// __DMB();
 }
 
 inline void clean_and_invalidate_dcache_by_range(void *addr, int32_t size) {
