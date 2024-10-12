e31c95e20c5037566b7ac0ecfeaba30c744e2710
Dan Green <danngreen1@gmail.com>
2024-10-11 17:04:44 -0700

    Add more register make()

--- drivers/adc_i2c_tla2528_registers.hh
+++ drivers/adc_i2c_tla2528_registers.hh
@@ -16,9 +16,6 @@ struct SystemStatus : ReadWrite {
 	uint8_t : 7;
 	uint8_t RsvdAlwaysHigh : 1;
 
-	// constexpr operator uint8_t() {
-	// 	return (RevID << 0) | (ProductID << 2) | (VersionID << 4);
-	// }
 	constexpr static SystemStatus make(uint8_t raw) {
 		return {
 			.RsvdAlwaysHigh = Bits<7>(raw),
@@ -35,21 +32,12 @@ struct GeneralCfg : ReadWrite {
 	uint8_t StartConversion : 1; //W only
 	uint8_t : 4;
 
-	// constexpr operator uint8_t() {
-	// 	return (PullDownCC1 << 0) | (PullDownCC2 << 1) | (MeasureCC1 << 2) | (MeasureCC2 << 3) |
-	// 		   (ConnectVConnCC1 << 4) | (ConnectVConnCC2 << 5) | (PullUpCC1 << 6) | (PullUpCC2 << 7);
-	// }
-
-	// constexpr static Switches0 make(uint8_t raw) {
-	// 	return {.PullDownCC1 = Bits<0>(raw),
-	// 			.PullDownCC2 = Bits<1>(raw),
-	// 			.MeasureCC1 = Bits<2>(raw),
-	// 			.MeasureCC2 = Bits<3>(raw),
-	// 			.ConnectVConnCC1 = Bits<4>(raw),
-	// 			.ConnectVConnCC2 = Bits<5>(raw),
-	// 			.PullUpCC1 = Bits<6>(raw),
-	// 			.PullUpCC2 = Bits<7>(raw)};
-	// }
+	constexpr static GeneralCfg make(uint8_t raw) {
+		return {.Reset = Bits<0>(raw),
+				.Calibrate = Bits<1>(raw),
+				.ForceChannelsAnalog = Bits<2>(raw),
+				.StartConversion = Bits<3>(raw)};
+	}
 };
 
 struct DataCfg : ReadWrite {
@@ -60,18 +48,9 @@ struct DataCfg : ReadWrite {
 	uint8_t : 1;
 	uint8_t DebugOutput0xA5A : 1;
 
-	// constexpr operator uint8_t() {
-	// 	return (EnableTXCC1 << 0) | (EnableTXCC2 << 1) | (AutoCRC << 2) | (DataRoleSrc << 4) | (SpecRev << 5) |
-	// 		   (PowerRoleSrc << 7);
-	// }
-	// constexpr static Switches1 make(uint8_t raw) {
-	// 	return {.EnableTXCC1 = Bits<0>(raw),
-	// 			.EnableTXCC2 = Bits<1>(raw),
-	// 			.AutoCRC = Bits<2>(raw),
-	// 			.DataRoleSrc = Bits<4>(raw),
-	// 			.SpecRev = Bits<5, 6>(raw),
-	// 			.PowerRoleSrc = Bits<7>(raw)};
-	// }
+	constexpr static DataCfg make(uint8_t raw) {
+		return {.AppendStatus = Bits<4, 5>(raw), .DebugOutput0xA5A = Bits<7>(raw)};
+	}
 };
 
 struct OversampleCfg : ReadWrite {
@@ -80,15 +59,11 @@ struct OversampleCfg : ReadWrite {
 	uint8_t OversampleBits : 3;
 	uint8_t : 5;
 
-	// constexpr operator uint8_t() {
-	// 	return (DAC << 0) | (MeasureVBUS << 6);
-	// }
-	// constexpr static Measure make(uint8_t raw) {
-	// 	return {
-	// 		.DAC = Bits<0, 1, 2, 3, 4, 5>(raw),
-	// 		.MeasureVBUS = Bits<6>(raw),
-	// 	};
-	// }
+	constexpr static OversampleCfg make(uint8_t raw) {
+		return {
+			.OversampleBits = Bits<0, 1, 2>(raw),
+		};
+	}
 };
 
 struct OpMode : ReadWrite {
@@ -100,15 +75,12 @@ struct OpMode : ReadWrite {
 
 	enum OscSels { OscSelLowPower = 1, OscSelHighSpeed = 0 };
 
-	// constexpr operator uint8_t() {
-	// 	return (BMCSlicerDAC << 0) | (SliderDACHys << 6);
-	// }
-	// constexpr static Slice make(uint8_t raw) {
-	// 	return {
-	// 		.BMCSlicerDAC = Bits<0, 1, 2, 3, 4, 5>(raw),
-	// 		.SliderDACHys = Bits<6, 7>(raw),
-	// 	};
-	// }
+	constexpr static OpMode make(uint8_t raw) {
+		return {
+			.ClockDiv = Bits<0, 1, 2, 3>(raw),
+			.OscSel = Bits<4>(raw),
+		};
+	}
 };
 
 struct PinCfg : ReadWrite {
@@ -116,16 +88,9 @@ struct PinCfg : ReadWrite {
 
 	uint8_t BitMaskDigitalGPIO; //bitmask 1 = GPIO, 0 = analog
 
-	// constexpr operator uint8_t() {
-	// 	return (AutoStartOnCRCRx << 1) | (HostCurrent << 2) | (MaskAllInt << 5);
-	// }
-	// constexpr static Control0 make(uint8_t raw) {
-	// 	return {
-	// 		.AutoStartOnCRCRx = Bits<1>(raw),
-	// 		.HostCurrent = Bits<2, 3>(raw),
-	// 		.MaskAllInt = Bits<5>(raw),
-	// 	};
-	// }
+	constexpr static PinCfg make(uint8_t raw) {
+		return PinCfg{.BitMaskDigitalGPIO = raw};
+	}
 };
 
 struct GPIOCfg : WriteClear {
@@ -133,30 +98,19 @@ struct GPIOCfg : WriteClear {
 
 	uint8_t BitMaskOutputs; //bitmask 1 = output, 0 = input
 
-	// constexpr static Control0WC make(uint8_t raw) {
-	// 	return {
-	// 		.TXStart = Bits<0>(raw),
-	// 		.TXFlush = Bits<6>(raw),
-	// 	};
-	// }
+	constexpr static GPIOCfg make(uint8_t raw) {
+		return GPIOCfg{.BitMaskOutputs = raw};
+	}
 };
 
-struct GPOCgf : ReadWrite {
+struct GPOCfg : ReadWrite {
 	enum : uint8_t { Address = 0x09 };
 
 	uint8_t BitMaskOutputsPushPull; //bitmask: 1 = push-pull, 0 = open-drain
 
-	// constexpr operator uint8_t() {
-	// 	return (EnableSOP1 << 0) | (EnableSOP2 << 1) | (RXFlush << 2) | (BISTMode2 << 4) | (EnableSOP1Dbg << 5) |
-	// 		   (EnableSOP2Dbg << 6);
-	// }
-	// constexpr static Control1 make(uint8_t raw) {
-	// 	return {.EnableSOP1 = Bits<0>(raw),
-	// 			.EnableSOP2 = Bits<1>(raw),
-	// 			.BISTMode2 = Bits<4>(raw),
-	// 			.EnableSOP1Dbg = Bits<5>(raw),
-	// 			.EnableSOP2Dbg = Bits<6>(raw)};
-	// }
+	constexpr static GPOCfg make(uint8_t raw) {
+		return {.BitMaskOutputsPushPull = raw};
+	}
 };
 
 struct GPOValue : ReadWrite {
@@ -164,16 +118,9 @@ struct GPOValue : ReadWrite {
 
 	uint8_t BitMaskSetOutput;
 
-	// constexpr operator uint8_t() {
-	// 	return (Toggle << 0) | (PollingMode << 1) | (WakeEnable << 3) | (ToggleIgnoreRa << 5) | (ToggleTime << 6);
-	// }
-	// constexpr static Control2 make(uint8_t raw) {
-	// 	return {.Toggle = Bits<0>(raw),
-	// 			.PollingMode = Bits<1, 2>(raw),
-	// 			.WakeEnable = Bits<3>(raw),
-	// 			.ToggleIgnoreRa = Bits<5>(raw),
-	// 			.ToggleTime = Bits<6, 7>(raw)};
-	// }
+	constexpr static GPOValue make(uint8_t raw) {
+		return {.BitMaskSetOutput = raw};
+	}
 };
 
 struct GPIValue : ReadOnly {
@@ -181,17 +128,9 @@ struct GPIValue : ReadOnly {
 
 	uint8_t BitMaskReadInput;
 
-	// constexpr operator uint8_t() {
-	// 	return (AutoRetryCRC << 0) | (NumRetries << 1) | (AutoSoftReset << 3) | (AutoHardReset << 4) |
-	// 		   (BISTTMode << 5) | (SendHardReset << 6);
-	// }
-	// constexpr static Control3 make(uint8_t raw) {
-	// 	return {.AutoRetryCRC = Bits<0>(raw),
-	// 			.NumRetries = Bits<1, 2>(raw),
-	// 			.AutoSoftReset = Bits<3>(raw),
-	// 			.AutoHardReset = Bits<4>(raw),
-	// 			.BISTTMode = Bits<5>(raw)};
-	// }
+	constexpr static GPIValue make(uint8_t raw) {
+		return {.BitMaskReadInput = raw};
+	}
 };
 
 struct SequenceCfg : ReadWrite {
@@ -203,20 +142,13 @@ struct SequenceCfg : ReadWrite {
 	uint8_t SeqStart : 1;
 	uint8_t : 3;
 
-	// constexpr operator uint8_t() {
-	// 	return (HostCurrentReq << 0) | (Collision << 1) | (Wake << 2) | (Alert << 3) | (CRCCheck << 4) |
-	// 		   (CompChange << 5) | (CCBusActivity << 6) | (VBusOK << 7);
-	// }
-	// constexpr static Mask make(uint8_t raw) {
-	// 	return {.HostCurrentReq = Bits<0>(raw),
-	// 			.Collision = Bits<1>(raw),
-	// 			.Wake = Bits<2>(raw),
-	// 			.Alert = Bits<3>(raw),
-	// 			.CRCCheck = Bits<4>(raw),
-	// 			.CompChange = Bits<5>(raw),
-	// 			.CCBusActivity = Bits<6>(raw),
-	// 			.VBusOK = Bits<7>(raw)};
-	// }
+	constexpr static SequenceCfg make(uint8_t raw) {
+		return {
+			.SeqModeAuto = Bits<0>(raw),
+			.SeqModeReserved = Bits<1>(raw),
+			.SeqStart = Bits<4>(raw),
+		};
+	}
 };
 
 struct ChannelSel : ReadWrite {
@@ -226,17 +158,11 @@ struct ChannelSel : ReadWrite {
 	uint8_t : 1;
 	uint8_t : 4;
 
-	// constexpr operator uint8_t() {
-	// 	return (BandGapAndWake << 0) | (MeasureBlock << 1) | (RXAndCurrentRefs << 2) | (IntOsc << 3);
-	// }
-	// constexpr static Power make(uint8_t raw) {
-	// 	return {
-	// 		.BandGapAndWake = Bits<0>(raw),
-	// 		.MeasureBlock = Bits<1>(raw),
-	// 		.RXAndCurrentRefs = Bits<2>(raw),
-	// 		.IntOsc = Bits<3>(raw),
-	// 	};
-	// }
+	constexpr static ChannelSel make(uint8_t raw) {
+		return {
+			.ManualChannelSel = Bits<0, 1, 2>(raw),
+		};
+	}
 };
 
 struct AutoSeqChanSel : WriteClear {
@@ -244,9 +170,9 @@ struct AutoSeqChanSel : WriteClear {
 
 	uint8_t BitMaskChannelEnabled;
 
-	// constexpr operator uint8_t() {
-	// 	return (SWReset << 0) | (PDReset << 1);
-	// }
+	constexpr static AutoSeqChanSel make(uint8_t raw) {
+		return {.BitMaskChannelEnabled = raw};
+	}
 };
 
 } // namespace mdrivlib::TLA2528
