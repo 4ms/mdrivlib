7b05c774381550f7274369d2f0ff8b5b6ad2824f
Dan Green <danngreen1@gmail.com>
2024-10-12 18:09:19 -0700

    Finish TLA2528 registers

--- drivers/adc_i2c_tla2528_registers.hh
+++ drivers/adc_i2c_tla2528_registers.hh
@@ -4,6 +4,15 @@
 
 namespace mdrivlib::TLA2528
 {
+
+enum Operation {
+	ReadReg = 0b0001'0000,
+	WriteReg = 0b0000'1000,
+	SetBits = 0b0001'1000,
+	ClearBits = 0b0010'0000,
+	ReadMultipleReg = 0b0011'0000,
+	WriteMultipleReg = 0b0010'1000,
+};
 using BusReg::Bits;
 using BusReg::ReadClear;
 using BusReg::ReadOnly;
@@ -16,6 +25,10 @@ struct SystemStatus : ReadWrite {
 	uint8_t : 7;
 	uint8_t RsvdAlwaysHigh : 1;
 
+	constexpr operator uint8_t() {
+		return (RsvdAlwaysHigh << 7);
+	}
+
 	constexpr static SystemStatus make(uint8_t raw) {
 		return {
 			.RsvdAlwaysHigh = Bits<7>(raw),
@@ -26,11 +39,15 @@ struct SystemStatus : ReadWrite {
 struct GeneralCfg : ReadWrite {
 	enum : uint8_t { Address = 0x01 };
 
-	uint8_t Reset : 1; //W only
+	uint8_t Reset : 1;
 	uint8_t Calibrate : 1;
 	uint8_t ForceChannelsAnalog : 1;
-	uint8_t StartConversion : 1; //W only
+	uint8_t StartConversion : 1;
 	uint8_t : 4;
+
+	operator uint8_t() {
+		return (Reset << 0) | (Calibrate << 1) | (ForceChannelsAnalog << 2) | (StartConversion << 3);
+	}
 
 	constexpr static GeneralCfg make(uint8_t raw) {
 		return {.Reset = Bits<0>(raw),
@@ -48,6 +65,10 @@ struct DataCfg : ReadWrite {
 	uint8_t : 1;
 	uint8_t DebugOutput0xA5A : 1;
 
+	operator uint8_t() {
+		return (AppendStatus << 4) | (DebugOutput0xA5A << 7);
+	}
+
 	constexpr static DataCfg make(uint8_t raw) {
 		return {.AppendStatus = Bits<4, 5>(raw), .DebugOutput0xA5A = Bits<7>(raw)};
 	}
@@ -58,6 +79,10 @@ struct OversampleCfg : ReadWrite {
 
 	uint8_t OversampleBits : 3;
 	uint8_t : 5;
+
+	operator uint8_t() {
+		return (OversampleBits << 0);
+	}
 
 	constexpr static OversampleCfg make(uint8_t raw) {
 		return {
@@ -72,6 +97,10 @@ struct OpMode : ReadWrite {
 	uint8_t ClockDiv : 4;
 	uint8_t OscSel : 1;
 	uint8_t : 3;
+
+	operator uint8_t() {
+		return (ClockDiv << 0) | (OscSel << 4);
+	}
 
 	enum OscSels { OscSelLowPower = 1, OscSelHighSpeed = 0 };
 
@@ -88,6 +117,10 @@ struct PinCfg : ReadWrite {
 
 	uint8_t BitMaskDigitalGPIO; //bitmask 1 = GPIO, 0 = analog
 
+	operator uint8_t() {
+		return BitMaskDigitalGPIO;
+	}
+
 	constexpr static PinCfg make(uint8_t raw) {
 		return PinCfg{.BitMaskDigitalGPIO = raw};
 	}
@@ -97,6 +130,10 @@ struct GPIOCfg : WriteClear {
 	enum : uint8_t { Address = 0x07 };
 
 	uint8_t BitMaskOutputs; //bitmask 1 = output, 0 = input
+
+	operator uint8_t() {
+		return BitMaskOutputs;
+	}
 
 	constexpr static GPIOCfg make(uint8_t raw) {
 		return GPIOCfg{.BitMaskOutputs = raw};
@@ -108,6 +145,10 @@ struct GPOCfg : ReadWrite {
 
 	uint8_t BitMaskOutputsPushPull; //bitmask: 1 = push-pull, 0 = open-drain
 
+	operator uint8_t() {
+		return BitMaskOutputsPushPull;
+	}
+
 	constexpr static GPOCfg make(uint8_t raw) {
 		return {.BitMaskOutputsPushPull = raw};
 	}
@@ -118,6 +159,10 @@ struct GPOValue : ReadWrite {
 
 	uint8_t BitMaskSetOutput;
 
+	operator uint8_t() {
+		return BitMaskSetOutput;
+	}
+
 	constexpr static GPOValue make(uint8_t raw) {
 		return {.BitMaskSetOutput = raw};
 	}
@@ -127,6 +172,10 @@ struct GPIValue : ReadOnly {
 	enum : uint8_t { Address = 0x0d };
 
 	uint8_t BitMaskReadInput;
+
+	operator uint8_t() {
+		return BitMaskReadInput;
+	}
 
 	constexpr static GPIValue make(uint8_t raw) {
 		return {.BitMaskReadInput = raw};
@@ -141,6 +190,10 @@ struct SequenceCfg : ReadWrite {
 	uint8_t : 2;
 	uint8_t SeqStart : 1;
 	uint8_t : 3;
+
+	operator uint8_t() {
+		return (SeqModeAuto << 0) | (SeqModeReserved << 1) | (SeqStart << 4);
+	}
 
 	constexpr static SequenceCfg make(uint8_t raw) {
 		return {
@@ -158,6 +211,10 @@ struct ChannelSel : ReadWrite {
 	uint8_t : 1;
 	uint8_t : 4;
 
+	operator uint8_t() {
+		return (ManualChannelSel << 0);
+	}
+
 	constexpr static ChannelSel make(uint8_t raw) {
 		return {
 			.ManualChannelSel = Bits<0, 1, 2>(raw),
@@ -170,6 +227,10 @@ struct AutoSeqChanSel : WriteClear {
 
 	uint8_t BitMaskChannelEnabled;
 
+	operator uint8_t() {
+		return BitMaskChannelEnabled;
+	}
+
 	constexpr static AutoSeqChanSel make(uint8_t raw) {
 		return {.BitMaskChannelEnabled = raw};
 	}
@@ -181,15 +242,25 @@ struct AutoSeqChanSel : WriteClear {
 // First, manually created the bitfield structs.
 //
 // Create operator uint8_t() for bitfields with all 1-bit fields:
-//%s/^struct .*\zs\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << 1) | (\3 << 2) | (\4 << 3) | (\5 << 4) | (\6 << 5) | (\7 << 6) | (\8 << 7); }/
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << 1) | (\3 << 2) | (\4 << 3) | (\5 << 4) | (\6 << 5) | (\7 << 6) | (\8 << 7); }\r\r/
+
 // Create operator uint8_t for bitfields with seven fields. Must fill in shifts manually
-// s/^struct .*\zs\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\ze};/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X) | (\6 << X) | (\7 << X); }/
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X) | (\6 << X) | (\7 << X); }\r\r/
+
 // Create operator uint8_t for bitfields with six fields. Must fill in shifts manually
-// s/^struct .*\zs\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\ze};/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X) | (\6 << X); }/
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X) | (\6 << X); }\r\r/
+
 // Create operator uint8_t for bitfields with five fields. Must fill in shifts manually
-// s/^struct .*\zs\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\ze};/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X); }/
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X) | (\5 << X); }\r\r/
+
 // Create operator uint8_t for bitfields with four fields. Must fill in shifts manually
-// s/^struct .*\zs\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\tuint8_t \(.*\) : \d;\n\ze};/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X); }/
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) | (\4 << X); }\r\r/
+
+// Three fields
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) | (\3 << X) }\r\r/
+
+// %s/^struct .*\n.*\n\zs\n\tuint8_t \(.*\): \d;\n\tuint8_t \(.*\): \d;\n\n\ze/\0\r\toperator uint8_t() { return (\1 << 0) | (\2 << X) }\r\r/
+
 // Three, two, and one fields are easy to do manually (typically have one or more _res)
 // Afterwards, remove all (<< \d);
 // %s/ | (<< \d)//g
