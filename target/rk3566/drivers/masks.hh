1fbfde8e97223ba993b102379e8161d79584fdd1
Dan Green <danngreen1@gmail.com>
2025-03-25 13:14:06 -0700

    Mask helpers are inline

--- target/rk3566/drivers/masks.hh
+++ target/rk3566/drivers/masks.hh
@@ -1,16 +1,18 @@
-#include <stdint.h> // clangd complains about cstdint????
+#pragma once
+#include <cstdint>
 
 namespace RockchipPeriph
 {
-static uint32_t masked_set_bit(uint8_t bit) {
+
+inline uint32_t masked_set_bit(uint8_t bit) {
 	return (1 << bit) | (1 << (bit + 16));
 }
 
-static uint32_t masked_clr_bit(uint8_t bit) {
+inline uint32_t masked_clr_bit(uint8_t bit) {
 	return (0 << bit) | (1 << (bit + 16));
 }
 
-static uint32_t masked(uint16_t mask, uint16_t bits) {
+inline uint32_t masked(uint16_t mask, uint16_t bits) {
 	return (mask << 16) | bits;
 }
 
