12026c8e5aa080be966d43c347686118cea9fec7
Dan Green <danngreen1@gmail.com>
2025-09-15 13:18:31 -0700

    Pin: fix bad access if GPIO is Unused

--- drivers/pin.cc
+++ drivers/pin.cc
@@ -46,10 +46,12 @@ void Pin::_init(PinMode mode, uint8_t af, PinPull pull, PinSpeed speed, PinOType
 }
 
 void Pin::high() const {
-	LL_GPIO_SetOutputPin(GPIOPort(port_), pin_);
+	if (port_ != GPIO::Unused)
+		LL_GPIO_SetOutputPin(GPIOPort(port_), pin_);
 }
 void Pin::low() const {
-	LL_GPIO_ResetOutputPin(GPIOPort(port_), pin_);
+	if (port_ != GPIO::Unused)
+		LL_GPIO_ResetOutputPin(GPIOPort(port_), pin_);
 }
 void Pin::on() const {
 	if (polarity_ == PinPolarity::Normal)
@@ -70,32 +72,43 @@ void Pin::set_to(uint32_t v) {
 		off();
 }
 void Pin::toggle() const {
-	LL_GPIO_TogglePin(GPIOPort(port_), pin_);
+	if (port_ != GPIO::Unused)
+		LL_GPIO_TogglePin(GPIOPort(port_), pin_);
 }
 
 bool Pin::read_raw() {
-	return LL_GPIO_IsInputPinSet(GPIOPort(port_), pin_) ? true : false;
+	if (port_ != GPIO::Unused)
+		return LL_GPIO_IsInputPinSet(GPIOPort(port_), pin_) ? true : false;
+	else
+		return false;
 }
 uint8_t Pin::is_on() {
 	return (polarity_ == PinPolarity::Normal) ? read_raw() : !read_raw();
 }
 
 void Pin::set_mode(PinMode mode) {
-	LL_GPIO_SetPinMode(GPIOPort(port_), pin_, HALParam(mode));
+	if (port_ != GPIO::Unused)
+		LL_GPIO_SetPinMode(GPIOPort(port_), pin_, HALParam(mode));
 }
 void Pin::set_speed(PinSpeed speed) {
-	LL_GPIO_SetPinSpeed(GPIOPort(port_), pin_, HALParam(speed));
+	if (port_ != GPIO::Unused)
+		LL_GPIO_SetPinSpeed(GPIOPort(port_), pin_, HALParam(speed));
 }
 void Pin::set_pull(PinPull pull) {
-	LL_GPIO_SetPinPull(GPIOPort(port_), pin_, HALParam(pull));
+	if (port_ != GPIO::Unused)
+		LL_GPIO_SetPinPull(GPIOPort(port_), pin_, HALParam(pull));
 }
 void Pin::set_alt(uint8_t af) {
+	if (port_ == GPIO::Unused)
+		return;
+
 	if (pin_ >= LL_GPIO_PIN_8)
 		LL_GPIO_SetAFPin_8_15(GPIOPort(port_), pin_, af);
 	else
 		LL_GPIO_SetAFPin_0_7(GPIOPort(port_), pin_, af);
 }
 void Pin::set_otype(PinOType otype) {
-	LL_GPIO_SetPinOutputType(GPIOPort(port_), pin_, HALParam(otype));
+	if (port_ != GPIO::Unused)
+		LL_GPIO_SetPinOutputType(GPIOPort(port_), pin_, HALParam(otype));
 }
 } // namespace mdrivlib
