df6354062317b026dfbc4bd74776718cf1cd9ff5
Dan Green <danngreen1@gmail.com>
2025-11-06 15:10:48 -0800

    Add SAI PDM driver

---
+++ target/stm32mp1/drivers/sai_pdm.hh
@@ -0,0 +1,67 @@
+#pragma once
+#include "drivers/interrupt.hh"
+#include "drivers/pin.hh"
+#include "drivers/sai_config_struct.hh"
+
+namespace mdrivlib
+{
+class SaiPdmPeriph {
+public:
+	enum Error {
+		SAI_NO_ERR,
+		SAI_INIT_ERR,
+		SAI_I2C_ERR,
+		SAI_XMIT_ERR,
+	};
+
+	SaiPdmPeriph(const SaiConfig &def)
+		: saidef_(def) {
+	}
+
+	~SaiPdmPeriph() = default;
+
+	Error init();
+	void set_rx_buffer_start(uint8_t *rx_buf_ptr, uint32_t block_size);
+	void set_rx_buffer(uint8_t *tx_buf_ptr, uint32_t frames_per_block);
+	void set_callbacks(Interrupt::ISRType &&rx_complete_cb, Interrupt::ISRType &&rx_half_complete_cb);
+
+	void start();
+	void stop();
+
+	void pause();
+	void resume();
+
+	Error change_samplerate_blocksize(uint32_t sample_rate, uint32_t block_size);
+
+	uint32_t te_errors = 0;
+	uint32_t fe_errors = 0;
+	uint32_t dme_errors = 0;
+
+private:
+	SaiConfig saidef_;
+	DMA_HandleTypeDef hdma_rx;
+	SAI_HandleTypeDef hsai_rx;
+	IRQn_Type rx_irqn;
+	uint8_t *rx_buf_ptr_;
+	uint32_t rx_block_size_;
+
+	void _init_pins();
+	Error _config_rx_sai();
+	void _config_rx_dma();
+	Error _init_sai_dma();
+	void _sai_enable(SAI_HandleTypeDef *hsai);
+	void _sai_disable(SAI_HandleTypeDef *hsai);
+	void _start_irq(IRQn_Type irqn);
+
+	Interrupt::ISRType rx_tc_cb;
+	Interrupt::ISRType rx_ht_cb;
+
+	uint32_t dma_tc_flag_index;
+	uint32_t dma_ht_flag_index;
+	uint32_t dma_te_flag_index;
+	uint32_t dma_fe_flag_index;
+	uint32_t dma_dme_flag_index;
+	volatile uint32_t *dma_isr_reg;
+	volatile uint32_t *dma_ifcr_reg;
+};
+} // namespace mdrivlib
