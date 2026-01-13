50fc38769d7e970f8cfc35f024322337acc31795
Dan Green <danngreen1@gmail.com>
2026-01-13 14:07:09 -0800

    mp13x uart: manually enable FIFO

--- target/stm32mp13/drivers/uart_target.hh
+++ target/stm32mp13/drivers/uart_target.hh
@@ -100,6 +100,16 @@ struct UartTarget {
 			if (err != HAL_OK) {
 				// __BKPT(43);
 			}
+			// disable peripheral and wait for completion
+			hal_h.Instance->CR1 &= ~USART_CR1_UE;
+			while (hal_h.Instance->CR1 & USART_CR1_UE)
+				;
+
+			//HAL_USART_Init() does not enable Fifo Mode, so we must do it manually
+			hal_h.Instance->CR1 |= USART_CR1_FIFOEN;
+
+			// enable peripheral
+			hal_h.Instance->CR1 |= USART_CR1_UE;
 		}
 	}
 
