dc273e9c8a8569b94b526b3f7306c38c5662fccf
Dan Green <danngreen1@gmail.com>
2026-02-10 16:09:12 -0800

    Default codec init

--- drivers/codec_TLV320AIC3204.cc
+++ drivers/codec_TLV320AIC3204.cc
@@ -33,10 +33,43 @@ namespace mdrivlib
 {
 using namespace CodecTLV320AIC3204Register;
 
-constexpr std::array<Registers, 17> default_init{
+constexpr std::array<Registers, 42> default_init{
 	{
+		// PAGE 0
+		//////////////////////////////////
 		PageSelect{.page = 0},
+
 		SwReset{.Reset = 1},
+
+		//
+		// PIN MUX
+		//
+
+		// MCLK pin -> Codec clock input (B1)
+		ClockSettingMultiplexors{.CodecClkIn = ClockSettingMultiplexors::MCLK,
+								 .PLLInputClock = ClockSettingMultiplexors::PLL_MCLK,
+								 .HighPLLClockRange = 0},
+
+		// BCLK->I2S BCLK (C2)
+		// WCLK->I2S WCLK (E3)
+		AudioInterfaceSetting1{.DOUTHighImpedance = 0,
+							   .WCLKDirection = AudioInterfaceSetting1::Input,
+							   .BCLKDirection = AudioInterfaceSetting1::Input,
+							   .WordLength = AudioInterfaceSetting1::Bits32,
+							   .Interface = AudioInterfaceSetting1::I2S},
+
+		// DOUT->I2S DOUT (J5)
+		DOUTControl{.DOUTFunction = DOUTControl::DOUT_PrimaryDOUT},
+
+		// DIN->I2S DIN (I4)
+		DINControl{.DINFunction = DINControl::DIN_Enabled},
+
+		// Try:
+		// MFP3(TP12)->HP Detect (W6) P0_R56 D2-D1=00, P0_R67 D7=1
+
+		//
+		// ADC Setup
+		//
 
 		// Program and power up NADC and MADC
 		// CODEC_CLKIN = NADC * MADC * AOSR * ADC_FS, note: AOSR is set next (128)
@@ -56,41 +89,90 @@ constexpr std::array<Registers, 17> default_init{
 		// Select ADC PRB_R1
 		ADCProcessingBlock{.ProcessingBlock = ADCProcessingBlock::PRB_R1},
 
+		// NDAC/MDAC
+		ClockSettingNDACValues{.NDACValue = 1, .NDACPowerUp = 1},
+		ClockSettingMDACValues{.MDACValue = 2, .MDACPowerUp = 1},
+
+		// DOSR = 128
+		DACOSRSettingMSB{.DOSRMSB = 0},
+		DACOSRSettingLSB{.DOSRLSB = 0x80},
+
+		// DAC PB
+		DACProcessingBlock{.ProcessingBlock = DACProcessingBlock::PRB_P8}, //??
+
 		// Program Analog Blocks
-		// 	Set register Page to 1
+
+		// PAGE 1
+		//////////////////////////////////
+
+		PageSelect{.page = 1},
+
 		// 	Disable coarse AVdd generation
+		PowerConfiguration{.DisableWeakAVDDtoDVDD = 1},
+
 		// 	Enable Master Analog Power Control
+		LDOControl{.AVDDLDOPowerUp = 1, .AnalogBlocksDisabled = 0},
+
 		// 	Program Common Mode voltage
-		// 	Program PowerTune (PTM) mode
+		CommonModeControl{.FullChipCommonMode = CommonModeControl::CM_0_9V},
+
+		// 	Program PowerTune (PTM) mode R4=highest
+		ADCPowerTuneConfiguration{.PTMConfiguration = ADCPowerTuneConfiguration::PTM_R4},
+
 		// 	Program MicPGA startup delay
+		AnalogInputQuickCharging{.PowerUpTime = AnalogInputQuickCharging::Default},
+
 		// 	Program Reference fast charging
+		ReferencePowerUpConfiguration{.Configuration = ReferencePowerUpConfiguration::Auto40ms},
+
 		// 	Routing of inputs and common mode to ADC input
+		LeftMICPGAPositiveRouting{.IN1LtoLeftMICPGA = InputRoutings::Routed_20k},
+		LeftMICPGANegativeRouting{.CM1LtoLeftMICPGA = InputRoutings::Routed_20k},
+		RightMICPGAPositiveRouting{.IN1RtoRightMICPGA = InputRoutings::Routed_20k},
+		RightMICPGANegativeRouting{.CM1RtoRightMICPGA = InputRoutings::Routed_20k},
+
 		// 	Unmute analog PGAs and set analog gain
-		// Power Up ADC
-		// 	Set register Page to 0
+		LeftMICPGAVolume{.Volume = 0xc},
+		RightMICPGAVolume{.Volume = 0xc},
+
+		// HPLRoutingSelection{.LeftDACtoHPL = 1},
+		// HPRRoutingSelection{.RightDACtoHPR = 1},
+		LOLRoutingSelection{.LeftDACtoLOL = 1},
+		LORRoutingSelection{.RightDACtoLOR = 1},
+		PlaybackConfiguration1{.LeftDACPTM = PlaybackConfiguration1::PTM_P3_P4},
+		PlaybackConfiguration2{.RightDACPTM = PlaybackConfiguration1::PTM_P3_P4},
+
+		LOLDriverGain{.Gain = 0x0, .Mute = 0},
+		LORDriverGain{.Gain = 0x0, .Mute = 0},
+
+		OutputDriverPowerControl{
+			.MARPowerUp = 0, .MALPowerUp = 0, .LORPowerUp = 1, .LOLPowerUp = 1, .HPRPowerUp = 0, .HPLPowerUp = 0},
+
+		// PAGE 0
+		//////////////////////////////////
+
+		PageSelect{.page = 0},
+
 		// 	Power up ADC Channels
+		ADCChannelSetup{.RightADCPowerUp = 1, .LeftADCPowerUp = 1},
+
 		// 	Unmute digital volume control
+		ADCFineGainAdjust{.RightADCMute = 0, .LeftADCMute = 0},
 
-		// Defaults:
-		// MCLK pin -> Codec clock input (B1)
-		ClockSettingMultiplexors{.CodecClkIn = 0b00, .PLLInputClock = 0b00, .HighPLLClockRange = 0},
+		DACChannelSetup1{.DACSoftStep = DACChannelSetup1::SoftStepDisabled,
+						 .RightDACDataPath = DACChannelSetup1::DACNormal,
+						 .LeftDACDataPath = DACChannelSetup1::DACNormal,
+						 .RightDACPowerUp = 1,
+						 .LeftDACPowerUp = 1},
 
-		// BCLK->I2S BCLK (C2)
-		// WCLK->I2S WCLK (E3)
-		AudioInterfaceSetting1{.DOUTHighImpedance = 0,
-							   .WCLKDirection = 0,
-							   .BCLKDirection = 0,
-							   .WordLength = AudioInterfaceSetting1::Bits24,
-							   .Interface = AudioInterfaceSetting1::I2S},
+		DACChannelSetup2{.DACMasterVolumeControl = DACChannelSetup2::Independent,
+						 .RightDACMute = 0,
+						 .LeftDACMute = 0,
+						 .DACAutoMute = DACChannelSetup2::AutoMuteDisabled},
 
-		// DOUT->I2S DOUT (J5)
-		DOUTControl{.DOUTFunction = DOUTControl::DOUT_PrimaryDOUT},
+		LeftDACVolume{.Volume = 0x0},  //0 = 0dB
+		RightDACVolume{.Volume = 0x0}, //0 = 0dB
 
-		// DIN->I2S DIN (I4)
-		DINControl{.DINFunction = DINControl::DIN_Enabled},
-
-		// Try:
-		// MFP3(TP12)->HP Detect (W6) P0_R56 D2-D1=00, P0_R67 D7=1
 	},
 };
 
